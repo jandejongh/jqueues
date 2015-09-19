2db40115abf617eb53b4390964583a800682d0e7
Jan de Jongh <jfcmdejongh@gmail.com>
2015-09-19 10:56:09 +0000

    Added example for ParallelSimQueues.
    git-svn-id: https://jandejongh.dnsdojo.org:3690/jqueues/trunk@92 23c8eed1-2f9f-4d5d-b507-dd3313c27e4c

--- java/src/nl/jdj/jqueues/r4/composite/example/Main.java
+++ java/src/nl/jdj/jqueues/r4/composite/example/Main.java
@@ -8,8 +8,10 @@ import nl.jdj.jqueues.r4.AbstractSimJob;
 import nl.jdj.jqueues.r4.NonPreemptiveQueue;
 import nl.jdj.jqueues.r4.SimJob;
 import nl.jdj.jqueues.r4.SimQueue;
+import nl.jdj.jqueues.r4.composite.BlackParallelSimQueues;
 import nl.jdj.jqueues.r4.composite.BlackTandemSimQueue;
 import nl.jdj.jqueues.r4.composite.DelegateSimJobFactory;
+import nl.jdj.jqueues.r4.composite.SimQueueSelector;
 import nl.jdj.jsimulation.r4.SimEvent;
 import nl.jdj.jsimulation.r4.SimEventAction;
 import nl.jdj.jsimulation.r4.SimEventList;
@@ -190,24 +192,6 @@ public final class Main
   
   /** Main method.
    * 
-   * Creates a (reusable) event list, some queues and jobs and shows the main feature of the package.
-   * Results are sent to {@link System#out}.
-   * 
-   * <p>
-   * In order to understand the generated output for the different queue types, we note that
-   * to each concrete {@link NonPreemptiveQueue}, exactly 10 jobs, numbered 1 through 10 inclusive are submitted.
-   * The arrival time and the requested service time of each job are equal to the job index.
-   * In other words, job 1 arrives at t=1 and requests S=1, job 2 arrives at t=2 and requests S=2, etc.
-   * 
-   * <p>
-   * Despite the arrival scheme, all queue types except NONE and IS can actually serve all jobs until completion without
-   * interruption from t=1 onwards. Since for these cases, the total requested service time is 55 (1+2+3+...+10), the simulation will end
-   * at t=56 (since we "start" at t=1) having completed all jobs.
-   * For NONE, jobs arrive, but never leave.
-   * For IS, the only multi-server discipline implemented so far, the case is different, since IS can process multiple jobs at the
-   * same time, effectively increasing its overall capacity while doing so. It is easily verified that with IS,
-   * job n (arriving at t=n) leaves the queue at t=2*n. Hence, at t=20, job 10 departs and the simulation ends.
-   * 
    * @param args The command-line arguments (ignored).
    * 
    */
@@ -254,8 +238,47 @@ public final class Main
     }
     System.out.println ("-> Executing event list...");
     el.run ();
-    // System.out.println ("-> Resetting event list...");
-    // el.reset ();
+    System.out.println ("-> Resetting event list...");
+    el.reset ();
+    System.out.println ("-> Creating Parallel queue...");
+    final SimQueue parallelQueue = new BlackParallelSimQueues (el, set, delegateSimJobFactory, new SimQueueSelector<SimJob, SimJob, SimQueue> ()
+    {
+      boolean toSecond = false;
+      @Override
+      public SimQueue<SimJob, SimQueue> selectFirstQueue (double time, SimJob job)
+      {
+        final SimQueue dstQueue;
+        if (toSecond)
+          dstQueue = lcfsQueue;
+        else
+          dstQueue = fcfsQueue;
+        toSecond = ! toSecond;
+        return dstQueue;
+      }
+      @Override
+      public SimQueue<SimJob, SimQueue> selectNextQueue (double time, SimJob job, SimQueue previousQueue)
+      {
+        return null;
+      }
+    });
+    System.out.println ("-> Submitting jobs to Parallel queue...");
+    for (int i = 0; i < jobList.size (); i++)
+    {
+      final SimJob j = jobList.get (i);
+      final double arrTime = i + 1;
+      el.add (new SimEvent ("ARRIVAL_" + i + 1, i + 1, null, new SimEventAction ()
+      {
+        @Override
+        public void action (final SimEvent event)
+        {
+          parallelQueue.arrive (j, arrTime);
+        }
+      }));
+    }
+    System.out.println ("-> Executing event list...");
+    el.run ();
+    System.out.println ("-> Resetting event list...");
+    el.reset ();
     System.out.println ("=== FINISHED ===");
   }
   
