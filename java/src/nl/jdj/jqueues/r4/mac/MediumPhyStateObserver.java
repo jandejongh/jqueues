2847419dfdba9d4ad968e4a0fc04f392fdde9a76
Jan de Jongh <jfcmdejongh@gmail.com>
2015-09-20 21:05:47 +0000

    Added generics for the monitor and the observer types.
    git-svn-id: https://jandejongh.dnsdojo.org:3690/jqueues/trunk@102 23c8eed1-2f9f-4d5d-b507-dd3313c27e4c

--- java/src/nl/jdj/jqueues/r4/mac/MediumPhyStateObserver.java
+++ java/src/nl/jdj/jqueues/r4/mac/MediumPhyStateObserver.java
@@ -3,7 +3,7 @@ package nl.jdj.jqueues.r4.mac;
 /**
  *
  */
-public interface MediumPhyStateObserver
+public interface MediumPhyStateObserver<M extends MediumPhyStateMonitor, O extends MediumPhyStateObserver>
 {
 
   public void mediumPhyStateUpdate (double time, MediumPhyState mediumPhyState);
