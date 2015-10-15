afee5adc6b1fe3f76ebb1e67618aa834055f9e5d
Jan de Jongh <jfcmdejongh@gmail.com>
2015-10-15 19:26:02 +0000

    Progress;now showing #arrivals/drops/revocations/departures. Changed layout somewhat to accommodate for this.
    git-svn-id: https://jandejongh.dnsdojo.org:3690/jqueues/trunk@173 23c8eed1-2f9f-4d5d-b507-dd3313c27e4c

--- java/src/nl/jdj/jqueues/r4/swing/JSimQueue.java
+++ java/src/nl/jdj/jqueues/r4/swing/JSimQueue.java
@@ -43,7 +43,83 @@ implements SimQueueVacationListener
       setMinimumSize (new Dimension (120, 240));
     }
   }
+  
+  private class ArrivalInfo
+  {
+    private SimJob job = null;
+    private double time;
+    private int counter = 0;
+  }
 
+  private final ArrivalInfo arrivalInfo = new ArrivalInfo ();
+  
+  private final void setArrivalInfo (final double t, final SimJob job, final SimQueue queue)
+  {
+    if (queue == this.queue)
+    {
+      this.arrivalInfo.job = job;
+      this.arrivalInfo.time = t;
+      this.arrivalInfo.counter++;
+    }
+  }
+  
+  private class DropInfo
+  {
+    private SimJob job = null;
+    private double time;
+    private int counter = 0;
+  }
+
+  private final DropInfo dropInfo = new DropInfo ();
+  
+  private final void setDropInfo (final double t, final SimJob job, final SimQueue queue)
+  {
+    if (queue == this.queue)
+    {
+      this.dropInfo.job = job;
+      this.dropInfo.time = t;
+      this.dropInfo.counter++;
+    }
+  }
+  
+  private class RevocationInfo
+  {
+    private SimJob job = null;
+    private double time;
+    private int counter = 0;
+  }
+
+  private final RevocationInfo revocationInfo = new RevocationInfo ();
+  
+  private final void setRevocationInfo (final double t, final SimJob job, final SimQueue queue)
+  {
+    if (queue == this.queue)
+    {
+      this.revocationInfo.job = job;
+      this.revocationInfo.time = t;
+      this.revocationInfo.counter++;
+    }
+  }
+  
+  private class DepartureInfo
+  {
+    private SimJob job = null;
+    private double time;
+    private int counter = 0;
+  }
+
+  private final DepartureInfo departureInfo = new DepartureInfo ();
+  
+  private final void setDepartureInfo (final double t, final SimJob job, final SimQueue queue)
+  {
+    if (queue == this.queue)
+    {
+      this.departureInfo.job = job;
+      this.departureInfo.time = t;
+      this.departureInfo.counter++;
+    }
+  }
+  
   @Override
   protected void paintComponent (Graphics g)
   {
@@ -63,12 +139,24 @@ implements SimQueueVacationListener
     g2d.setStroke (new BasicStroke (4.0f));
     g2d.setColor (this.queue.isQueueAccessVacation () ? Color.red : Color.green);
     g2d.drawRect (middleX - 50, middleY - 100, 100, 200);
+    // Draw the arrival info.
+    g2d.setColor (Color.yellow.darker ());
+    g2d.drawString ("Ar:" + this.arrivalInfo.counter, middleX - 47, middleY - 80);
+    // Draw the departure info.
+    g2d.setColor (Color.green.darker ());
+    g2d.drawString ("De:" + this.departureInfo.counter, middleX - 2, middleY - 80);
+    // Draw the revocation info.
+    g2d.setColor (Color.orange.darker ());
+    g2d.drawString ("Re:" + this.revocationInfo.counter, middleX - 47, middleY - 60);
+    // Draw the drop info.
+    g2d.setColor (Color.red.darker ());
+    g2d.drawString ("Dr:" + this.dropInfo.counter, middleX -2, middleY - 60);
     // Draw arrowed line from wait queue to server.
     g2d.setColor (this.queue.getServerAccessCredits () > 0 ? Color.green : Color.red);
-    g2d.drawLine (middleX, middleY - 40, middleX, middleY - 10);
+    g2d.drawLine (middleX, middleY - 20, middleX, middleY + 10);
     // Draw number of server-access credits.
     if (this.queue.getServerAccessCredits () < Integer.MAX_VALUE)
-      g2d.drawString ("" + this.queue.getServerAccessCredits (), middleX + 10, middleY - 20);
+      g2d.drawString ("" + this.queue.getServerAccessCredits (), middleX + 10, middleY);
     // Draw wait queue.
     g2d.setColor (Color.black);
     final BasicStroke savedStroke = (BasicStroke) g2d.getStroke ();
@@ -80,17 +168,17 @@ implements SimQueueVacationListener
           savedStroke.getMiterLimit (),
           new float[] { 5f, 10f },
           0f));
-    g2d.drawLine (middleX - 30, middleY - 40, middleX + 30, middleY - 40);
-    g2d.drawLine (middleX - 30, middleY - 60, middleX - 30, middleY - 40);
-    g2d.drawLine (middleX + 30, middleY - 60, middleX + 30, middleY - 40);
+    g2d.drawLine (middleX - 30, middleY - 20, middleX + 30, middleY - 20);
+    g2d.drawLine (middleX - 30, middleY - 40, middleX - 30, middleY - 20);
+    g2d.drawLine (middleX + 30, middleY - 40, middleX + 30, middleY - 20);
     g2d.setStroke (savedStroke);
     // Draw number of jobs waiting.
-    g2d.drawString ("" + (this.queue.getNumberOfJobs () - this.queue.getNumberOfJobsExecuting ()), middleX - 10, middleY - 50);
+    g2d.drawString ("W:" + (this.queue.getNumberOfJobs () - this.queue.getNumberOfJobsExecuting ()), middleX - 20, middleY - 30);
     // Draw (single) server.
     g2d.setColor (Color.black);
-    g2d.drawOval (middleX - 30, middleY - 10, 60, 60);
+    g2d.drawOval (middleX - 30, middleY + 10, 60, 60);
     // Draw number of jobs executing.
-    g2d.drawString ("" + this.queue.getNumberOfJobsExecuting (), middleX - 10, middleY +30);    
+    g2d.drawString ("E:" + this.queue.getNumberOfJobsExecuting (), middleX - 20, middleY + 50);    
     // Draw queue name.
     g2d.setColor (Color.black);
     g2d.drawString (this.queue.toString (), middleX - 40, middleY + 90);
@@ -136,6 +224,18 @@ implements SimQueueVacationListener
   @Override
   public void notifyReset (final double oldTime, final SimQueue queue)
   {
+    this.arrivalInfo.job = null;
+    this.arrivalInfo.time = Double.NEGATIVE_INFINITY;
+    this.arrivalInfo.counter = 0;
+    this.dropInfo.job = null;
+    this.dropInfo.time = Double.NEGATIVE_INFINITY;
+    this.dropInfo.counter = 0;
+    this.revocationInfo.job = null;
+    this.revocationInfo.time = Double.NEGATIVE_INFINITY;
+    this.revocationInfo.counter = 0;    
+    this.departureInfo.job = null;
+    this.departureInfo.time = Double.NEGATIVE_INFINITY;
+    this.departureInfo.counter = 0;
     notifyQueueChanged (oldTime, queue);    
   }
   
@@ -148,6 +248,7 @@ implements SimQueueVacationListener
   @Override
   public void notifyArrival (final double t, final SimJob job, final SimQueue queue)
   {
+    setArrivalInfo (t, job, queue);
     notifyQueueChanged (t, queue);
   }
 
@@ -160,18 +261,21 @@ implements SimQueueVacationListener
   @Override
   public void notifyDrop (final double t, final SimJob job, final SimQueue queue)
   {
+    setDropInfo (t, job, queue);
     notifyQueueChanged (t, queue);
   }
 
   @Override
   public void notifyRevocation (final double t, final SimJob job, final SimQueue queue)
   {
+    setRevocationInfo (t, job, queue);
     notifyQueueChanged (t, queue);
   }
 
   @Override
   public void notifyDeparture (final double t, final SimJob job, final SimQueue queue)
   {
+    setDepartureInfo (t, job, queue);
     notifyQueueChanged (t, queue);
   }
 
