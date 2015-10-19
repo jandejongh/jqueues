d97b87196f74cd8a5c1fc11e09a161a39233ba4c
Jan de Jongh <jfcmdejongh@gmail.com>
2015-10-19 13:26:23 +0000

    Extracted TestJob from tests.
    git-svn-id: https://jandejongh.dnsdojo.org:3690/jqueues/trunk@202 23c8eed1-2f9f-4d5d-b507-dd3313c27e4c

--- java/test/nl/jdj/jqueues/r4/nonpreemptive/NonPreemptiveTest.java
+++ java/test/nl/jdj/jqueues/r4/nonpreemptive/NonPreemptiveTest.java
@@ -3,10 +3,10 @@ package nl.jdj.jqueues.r4.nonpreemptive;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import nl.jdj.jqueues.r4.AbstractSimJob;
 import nl.jdj.jqueues.r4.DefaultSimQueueVacationListener;
 import nl.jdj.jqueues.r4.SimJob;
 import nl.jdj.jqueues.r4.SimQueue;
+import nl.jdj.jqueues.r4.TestJob;
 import nl.jdj.jsimulation.r4.SimEvent;
 import nl.jdj.jsimulation.r4.SimEventAction;
 import nl.jdj.jsimulation.r4.SimEventList;
@@ -49,140 +49,6 @@ public class NonPreemptiveTest
   {
   }
 
-  public static class TestJob extends AbstractSimJob
-  {
-    
-    private final boolean reported;
-    
-    public final int n;
-    
-    public TestJob (boolean reported, int n)
-    {
-      if (n <= 0)
-        throw new IllegalArgumentException ();
-      this.reported = reported;
-      this.n = n;
-    }
-
-    public boolean arrived = false;
-    
-    public boolean started = false;
-    
-    public boolean dropped = false;
-    
-    public boolean departed = false;
-    
-    public double arrivalTime = 0.0;
-    
-    public double startTime = 0.0;
-    
-    public double dropTime = 0.0;
-    
-    public double departureTime = 0.0;
-    
-    @Override
-    public double getServiceTime (SimQueue queue) throws IllegalArgumentException
-    {
-      if (queue == null && getQueue () == null)
-        return 0.0;
-      else
-        return (double) n;
-    }
-    
-    public final SimEventAction<SimJob> QUEUE_ARRIVE_ACTION = new SimEventAction<SimJob> ()
-    {
-      @Override
-      public void action (final SimEvent event)
-      {
-        if (TestJob.this.reported)
-          System.out.println ("t = " + event.getTime () + ": Job " + TestJob.this.n + " arrives.");
-        if (TestJob.this.arrived)
-          fail ("Already arrived!");
-        TestJob.this.arrived = true;
-        TestJob.this.arrivalTime = event.getTime ();
-      }
-    };
-    
-    @Override
-    public SimEventAction<SimJob> getQueueArriveAction ()
-    {
-      return this.QUEUE_ARRIVE_ACTION;
-    }
-    
-    public final SimEventAction<SimJob> QUEUE_START_ACTION = new SimEventAction<SimJob> ()
-    {
-      @Override
-      public void action (final SimEvent event)
-      {
-        if (TestJob.this.reported)
-          System.out.println ("t = " + event.getTime () + ": Job " + TestJob.this.n + " starts.");      
-        if (TestJob.this.started)
-          fail ("Already started!");
-        if (! TestJob.this.arrived)
-          fail ("Starting before arrival!");
-        if (TestJob.this.departed)
-          fail ("Starting after departure!");
-        if (TestJob.this.dropped)
-          fail ("Starting after drop!");
-        TestJob.this.started = true;
-        TestJob.this.startTime = event.getTime ();
-      }
-    };
-    
-    @Override
-    public SimEventAction<SimJob> getQueueStartAction ()
-    {
-      return this.QUEUE_START_ACTION;
-    }
-
-    public final SimEventAction<SimJob> QUEUE_DROP_ACTION = new SimEventAction<SimJob> ()
-    {
-      @Override
-      public void action (final SimEvent event)
-      {
-        if (TestJob.this.reported)
-          System.out.println ("t = " + event.getTime () + ": Job " + TestJob.this.n + " dropped.");      
-        if (! TestJob.this.arrived)
-          fail ("Dropped before arrival!");
-        if (TestJob.this.departed)
-          fail ("Dropped after departure!");
-        TestJob.this.dropped = true;
-        TestJob.this.dropTime = event.getTime ();
-      }
-    };
-    
-    @Override
-    public SimEventAction<SimJob> getQueueDropAction ()
-    {
-      return this.QUEUE_DROP_ACTION;
-    }
-
-    public final SimEventAction<SimJob> QUEUE_DEPART_ACTION = new SimEventAction<SimJob> ()
-    {
-      @Override
-      public void action (final SimEvent event)
-      {
-        if (TestJob.this.reported)
-          System.out.println ("t = " + event.getTime () + ": Job " + TestJob.this.n + " departs.");      
-        if (TestJob.this.departed)
-          fail ("Already departed!");
-        if (! TestJob.this.arrived)
-          fail ("Departure before arrival!");
-        if (! TestJob.this.started)
-          fail ("Departure before start!");
-        TestJob.this.departed = true;
-        TestJob.this.departureTime = event.getTime ();
-      }
-    };
-    
-    @Override
-    public SimEventAction<SimJob> getQueueDepartAction ()
-    {
-      return this.QUEUE_DEPART_ACTION;
-    }
-
-  }
-  
   public static List<TestJob> scheduleJobArrivals
   (final boolean reported, final int n, final SimEventList eventList, final SimQueue queue)
   {
