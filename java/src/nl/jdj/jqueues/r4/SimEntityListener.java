e53f55f99931c765d37c79b79b31ab2d66480d62
Jan de Jongh <jfcmdejongh@gmail.com>
2015-11-02 17:12:11 +0000

    This is a commit for a major revision and extension of the jqueues package. The commit has been split in order to do proper reviews of the committed code.
    SimEntityListener / DefaultSimEntityListener / StdOutSimEntityListener.
    
    git-svn-id: https://jandejongh.dnsdojo.org:3690/jqueues/trunk@232 23c8eed1-2f9f-4d5d-b507-dd3313c27e4c

---
+++ java/src/nl/jdj/jqueues/r4/SimEntityListener.java
@@ -0,0 +1,72 @@
+package nl.jdj.jqueues.r4;
+
+/** A listener to one or multiple {@link SimEntity}s.
+ *
+ * @param <J> The type of {@link SimJob}s supported.
+ * @param <Q> The type of {@link SimQueue}s supported.
+ * 
+ */
+public interface SimEntityListener<J extends SimJob, Q extends SimQueue>
+{
+  
+  /** Notification of a reset at a {@link SimEntity}.
+   * 
+   * @param entity The entity that has been reset.
+   * 
+   */
+  public void notifyResetEntity (SimEntity entity);
+  
+  /** Notification of the arrival of a job at a queue.
+   * 
+   * The notification is issued immediately at the time a job arrives at a queue,
+   * in other words, at a point where the queue does not even know yet about the existence of the job
+   * (and vice versa, for that matter).
+   * 
+   * @param time  The (current) time.
+   * @param job   The job.
+   * @param queue The queue.
+   * 
+   * @see SimQueue#arrive
+   * 
+   */
+  public void notifyArrival (double time, J job, Q queue);
+  
+  /** Notification of the start of a job at a queue.
+   * 
+   * @param time  The (current) time.
+   * @param job   The job.
+   * @param queue The queue.
+   * 
+   */
+  public void notifyStart (double time, J job, Q queue);
+  
+  /** Notification of the drop of a job at a queue.
+   * 
+   * @param time  The (current) time.
+   * @param job   The job.
+   * @param queue The queue.
+   * 
+   */
+  public void notifyDrop (double time, J job, Q queue);
+  
+  /** Notification of the (successful) revocation of a job at a queue.
+   * 
+   * @param time  The (current) time.
+   * @param job   The job.
+   * @param queue The queue.
+   * 
+   * @see SimQueue#revoke
+   * 
+   */
+  public void notifyRevocation (double time, J job, Q queue);
+  
+  /** Notification of the departure of a job at a queue.
+   * 
+   * @param time  The (current) time.
+   * @param job   The job.
+   * @param queue The queue.
+   * 
+   */
+  public void notifyDeparture (double time, J job, Q queue);
+  
+}
