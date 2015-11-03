258d254e1cea92f7600be9127bc0ff5f20143bfc
Jan de Jongh <jfcmdejongh@gmail.com>
2015-11-03 00:52:17 +0000

    This is a commit for a major revision and extension of the jqueues package. The commit has been split in order to do proper reviews of the committed code.
    composite: parallel queues.
    
    git-svn-id: https://jandejongh.dnsdojo.org:3690/jqueues/trunk@249 23c8eed1-2f9f-4d5d-b507-dd3313c27e4c

---
+++ java/src/nl/jdj/jqueues/r4/composite/AbstractBlackParallelSimQueues.java
@@ -0,0 +1,241 @@
+package nl.jdj.jqueues.r4.composite;
+
+import java.util.Set;
+import nl.jdj.jqueues.r4.AbstractSimJob;
+import nl.jdj.jqueues.r4.SimJob;
+import nl.jdj.jqueues.r4.SimQueue;
+import nl.jdj.jsimulation.r4.SimEventList;
+
+/** Parallel queues (abstract).
+ * 
+ * Under the hood, a delegate job for each {@link SimJob} visits one and only one of the
+ * embedded {@link SimQueue}s, as controlled by a {@link SimQueueSelector}.
+ * 
+ * <p>
+ * After the delegate job departs from its internal queue, the "real" job departs
+ * from the {@link BlackParallelSimQueues}.
+ *
+ * @param <DJ> The delegate-job type.
+ * @param <DQ> The queue-type for delegate jobs.
+ * @param <J>  The job type.
+ * @param <Q>  The queue type for jobs.
+ * 
+ */
+public abstract class AbstractBlackParallelSimQueues
+<DJ extends AbstractSimJob, DQ extends SimQueue, J extends SimJob, Q extends AbstractBlackParallelSimQueues>
+  extends AbstractBlackSimQueueNetwork<DJ, DQ, J, Q>
+  implements BlackSimQueueNetwork<DJ, DQ, J, Q>
+{
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // CONSTRUCTOR(S)
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  /** Creates a (abstract) black parallel queue given an event list and a list of queues to put in parallel.
+   *
+   * @param eventList The event list to use.
+   * @param queues    The queues in no particular order.
+   * @param delegateSimJobFactory An optional factory for the delegate {@link SimJob}s.
+   * @param simQueueSelector An optional {@link SimQueueSelector} for arriving jobs; if <code>null</code>, this
+   *                         {@link BlackParallelSimQueues} must itself be a (and is selected as the)
+   *                         {@link SimQueueSelector}.
+   *
+   * @throws IllegalArgumentException If the event list is <code>null</code>,
+   *                                  the <code>queues</code> argument is <code>null</code>,
+   *                                  or if it contains a <code>null</code> entry,
+   *                                  or if no suitable <code>simQueueSelector</code> is found.
+   * 
+   * @see DelegateSimJobFactory
+   * @see DefaultDelegateSimJobFactory
+   * @see SimQueueSelector
+   * 
+   */
+  protected AbstractBlackParallelSimQueues
+  (final SimEventList eventList,
+    final Set<DQ> queues,
+    final DelegateSimJobFactory delegateSimJobFactory,
+    final SimQueueSelector simQueueSelector)
+  {
+    super (eventList, queues, delegateSimJobFactory);
+    if (simQueueSelector == null)
+    {
+      if (! (this instanceof SimQueueSelector))
+        throw new IllegalArgumentException ();
+      this.simQueueSelector = (SimQueueSelector) this;
+    }
+    else
+      this.simQueueSelector = simQueueSelector;
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // AbstractBlackSimQueueNetwork
+  // ABSTRACT METHODS FOR (SUB-)QUEUE SELECTION IN SUBCLASSES
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  /** Returns the first queue as obtained from the {@link SimQueueSelector}.
+   * 
+   * @return The first queue as obtained from the {@link SimQueueSelector}.
+   * 
+   * @see SimQueueSelector#selectFirstQueue
+   * 
+   */
+  @Override
+  protected final SimQueue<DJ, DQ> getFirstQueue (final double time, final J job)
+  {
+    return this.simQueueSelector.selectFirstQueue (time, job);
+  }
+
+  /** Returns {@code null}.
+   * 
+   * @throws IllegalStateException If the previous queue argument is <code>null</code> or not a member of {@link #getQueues}.
+   * 
+   */
+  @Override
+  protected final SimQueue<DJ, DQ> getNextQueue (final double time, final J job, final DQ previousQueue)
+  {
+    if (getQueues ().isEmpty ())
+      throw new IllegalStateException ();
+    if (previousQueue == null)
+      throw new IllegalStateException ();
+    if (! getQueues ().contains (previousQueue))
+      throw new IllegalStateException ();
+    return null;
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // SimQueueSelector
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  /** The {@link SimQueueSelector} for selecting the internal queue to visit.
+   * 
+   */
+  private final SimQueueSelector<J, DJ, DQ> simQueueSelector;
+  
+  /** Returns the {@link SimQueueSelector} for selecting the internal queue to visit.
+   * 
+   * @return The {@link SimQueueSelector} for selecting the internal queue to visit.
+   * 
+   */
+  protected final SimQueueSelector<J, DJ, DQ> getSimQueueSelector ()
+  {
+    return this.simQueueSelector;
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // DROP DESTINATION QUEUE
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  /** Calls super method (in order to make implementation final).
+   * 
+   */
+  @Override
+  protected final DQ getDropDestinationQueue (final double t, final DJ job, final DQ queue)
+  {
+    return super.getDropDestinationQueue (t, job, queue);
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // allowDelegateJobRevocations
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  /** Returns {@code false}.
+   * 
+   * @return {@code false}.
+   * 
+   */
+  @Override
+  protected final boolean getAllowDelegateJobRevocations ()
+  {
+    return false;
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // allowSubQueueAccessVacationChanges
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  /** Returns {@code false}.
+   * 
+   * @return {@code false}.
+   * 
+   */
+  @Override
+  protected final boolean getAllowSubQueueAccessVacationChanges ()
+  {
+    return false;
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // RESET
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  /** Calls super method (in order to make implementation final).
+   * 
+   * {@inheritDoc}
+   * 
+   */
+  @Override
+  public final void resetEntitySubClass ()
+  {
+    super.resetEntitySubClass ();
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // UPDATE
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  /** Calls super method (in order to make implementation final).
+   * 
+   */
+  @Override
+  public final void update (final double time)
+  {
+    super.update (time);
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // startForSubClass
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  /** Calls super method (in order to make implementation final).
+   * 
+   */
+  @Override
+  protected final void startForSubClass (final double time, final DJ job, final DQ queue)
+  {
+    super.startForSubClass (time, job, queue);
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // notifyNewNoWaitArmed
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  /** Calls super method (in order to make implementation final).
+   * 
+   */
+  @Override
+  public final void notifyNewNoWaitArmed (final double time, final DQ queue, final boolean noWaitArmed)
+  {
+    super.notifyNewNoWaitArmed (time, queue, noWaitArmed);
+  }
+  
+}
