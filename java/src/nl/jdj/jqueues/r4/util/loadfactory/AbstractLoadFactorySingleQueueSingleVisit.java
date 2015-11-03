1f3869a1769c16c338fc9a5249d5037ba5124b33
Jan de Jongh <jfcmdejongh@gmail.com>
2015-11-03 13:51:13 +0000

    This is a commit for a major revision and extension of the jqueues package. The commit has been split in order to do proper reviews of the committed code.
    loadfactory
    
    git-svn-id: https://jandejongh.dnsdojo.org:3690/jqueues/trunk@255 23c8eed1-2f9f-4d5d-b507-dd3313c27e4c

---
+++ java/src/nl/jdj/jqueues/r4/util/loadfactory/AbstractLoadFactorySingleQueueSingleVisit.java
@@ -0,0 +1,14 @@
+package nl.jdj.jqueues.r4.util.loadfactory;
+
+import nl.jdj.jqueues.r4.SimJob;
+import nl.jdj.jqueues.r4.SimQueue;
+
+/** Abstract implementation of {@link LoadFactorySingleQueueSingleVisit}, mostly meant to store utility methods common to
+ *  concrete implementations.
+ *
+ */
+public abstract class AbstractLoadFactorySingleQueueSingleVisit<J extends SimJob, Q extends SimQueue>
+implements LoadFactorySingleQueueSingleVisit<J, Q>
+{
+  
+}
---
+++ java/src/nl/jdj/jqueues/r4/util/loadfactory/pattern/LoadFactorySingleQueueSingleVisit_01.java
@@ -0,0 +1,94 @@
+package nl.jdj.jqueues.r4.util.loadfactory.pattern;
+
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import nl.jdj.jqueues.r4.SimJob;
+import nl.jdj.jqueues.r4.SimJobFactory;
+import nl.jdj.jqueues.r4.SimQueue;
+import nl.jdj.jqueues.r4.util.loadfactory.AbstractLoadFactorySingleQueueSingleVisit;
+import nl.jdj.jqueues.r4.util.loadfactory.LoadFactorySingleQueueSingleVisit;
+import nl.jdj.jqueues.r4.util.schedule.JobQueueVisitArrivalSchedule;
+import nl.jdj.jqueues.r4.util.schedule.QueueExternalEvent;
+import nl.jdj.jqueues.r4.util.schedule.Scheduler;
+import nl.jdj.jsimulation.r4.SimEventList;
+
+/** A concrete {@link LoadFactorySingleQueueSingleVisit}, pattern 01.
+ *
+ * @see #generate
+ * 
+ */
+public class LoadFactorySingleQueueSingleVisit_01<J extends SimJob, Q extends SimQueue>
+extends AbstractLoadFactorySingleQueueSingleVisit<J, Q>
+{
+
+  /** Creates a suitable map for the requested service time for a job visit to a queue.
+   * 
+   * @param queue The queue.
+   * @param n     The job number.
+   * 
+   * @return A map holding the service time (i.e., the job number) at the queue.
+   * 
+   * @see SimJobFactory#newInstance For the use of the map generated.
+   * 
+   */
+  protected Map<Q, Double> generateRequestedServiceTimeMap (final Q queue, final int n)
+  {
+    final Map<Q, Double> requestedServiceTimeMap = new HashMap ();
+    requestedServiceTimeMap.put (queue, (double) n);
+    return requestedServiceTimeMap;
+  }
+  
+  /** Generates the load.
+   * 
+   * <p>
+   * This method
+   * <ul>
+   * <li> generates the requested number of jobs, and number them starting with one;
+   * <li> set the requested service time for each job equal to its job number;
+   * <li> schedules a single arrival for each job at time equal to its job number.
+   * </ul>
+   * 
+   * <p>
+   * Jobs are returned in a {@link LinkedHashSet}, preserving the creation order of the jobs.
+   * 
+   * @see Scheduler#schedule
+   * 
+   */
+  @Override
+  public Set<J> generate
+  (final SimEventList eventList,
+    boolean attachSimJobsToEventList,
+    final Q queue,
+    final SimJobFactory<J, Q> jobFactory,
+    final int numberOfJobs,
+    final boolean reset,
+    final double resetTime,
+    final TreeMap<Double, LinkedHashSet<QueueExternalEvent<J, Q>>> queueExternalEvents)
+  {
+    if (eventList == null || queue == null || jobFactory == null)
+      throw new IllegalArgumentException ();
+    if (numberOfJobs < 0)
+      throw new IllegalArgumentException ();
+    final Set<J> jobs = new LinkedHashSet<> ();
+    final TreeMap<Double, LinkedHashSet<QueueExternalEvent<J, Q>>> realQueueExternalEvents =
+      ((queueExternalEvents != null) ? queueExternalEvents : new TreeMap<> ());
+    final Set<QueueExternalEvent> eventsToSchedule = new LinkedHashSet<> ();
+    final SimEventList jobEventList = (attachSimJobsToEventList ? eventList : null);
+    for (int i = 1; i <= numberOfJobs; i++)
+    {
+      final J job = jobFactory.newInstance (jobEventList, Integer.toString (i), generateRequestedServiceTimeMap (queue, i));
+      final QueueExternalEvent<J, Q> arrivalSchedule = new JobQueueVisitArrivalSchedule (job, queue, (double) i);
+      if (! realQueueExternalEvents.containsKey ((double) i))
+        realQueueExternalEvents.put ((double) i, new LinkedHashSet<> ());
+      realQueueExternalEvents.get ((double) i).add (arrivalSchedule);
+      eventsToSchedule.add (arrivalSchedule);
+      jobs.add (job);
+    }
+    Scheduler.schedule (eventList, reset, resetTime, eventsToSchedule);
+    return jobs;
+  }
+  
+}
