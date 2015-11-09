77ac439bf22e62371892c27663d6d91ef1119a76
Jan de Jongh <jfcmdejongh@gmail.com>
2015-11-09 22:11:29 +0000

    Added deadSilent and accuracy arguments.
    git-svn-id: https://jandejongh.dnsdojo.org:3690/jqueues/trunk@309 23c8eed1-2f9f-4d5d-b507-dd3313c27e4c

--- java/test/nl/jdj/jqueues/r4/DefaultSimQueueTests.java
+++ java/test/nl/jdj/jqueues/r4/DefaultSimQueueTests.java
@@ -16,14 +16,12 @@ import nl.jdj.jqueues.r4.util.loadfactory.pattern.KnownLoadFactory_SQ_SV;
 import nl.jdj.jsimulation.r4.SimEvent;
 import nl.jdj.jsimulation.r4.SimEventList;
 
-/**
+/** Default tests for {@link SimQueue}s.
  *
  */
 public class DefaultSimQueueTests
 {
 
-  public final static double ACCURACY = 1.0e-12;
-  
   public final static int NUMBER_OF_PASSES = 2;
   
   public static <Q extends SimQueue<DefaultVisitsLoggingSimJob, Q>> boolean doSimQueueTests_SQ_SV
@@ -31,22 +29,24 @@ public class DefaultSimQueueTests
      final SimQueuePredictor<DefaultVisitsLoggingSimJob, Q> predictor,
      final int numberOfJobs,
      final boolean silent,
+     final boolean deadSilent,
+     final double accuracy,
      final Set<KnownLoadFactory_SQ_SV> omit)
   throws SimQueuePredictionException
   {
-    if (queue == null || predictor == null || numberOfJobs < 0 || queue.getEventList () == null)
+    if (queue == null || predictor == null || numberOfJobs < 0 || queue.getEventList () == null || accuracy < 0)
       throw new IllegalArgumentException ();
     System.out.println ("========== SimQueue Tests [SQ/SV] ==============================================");
     System.out.println (queue);
     System.out.println ("================================================================================");
     final SimEventList<SimEvent> el = queue.getEventList ();
-    if ((! silent) && (queue instanceof AbstractSimQueue))
+    if ((! (silent || deadSilent)) && (queue instanceof AbstractSimQueue))
       ((AbstractSimQueue) queue).registerStdOutSimQueueListener ();
     for (final KnownLoadFactory_SQ_SV klf : KnownLoadFactory_SQ_SV.values ())
       if (omit == null || ! omit.contains (klf))
         for (int pass = 1; pass <= NUMBER_OF_PASSES; pass++)
         {
-          if (! silent)
+          if (! deadSilent)
             System.out.println ("===== Test: " + klf + ", pass " + pass + " =====");
           final SimJobFactory<DefaultVisitsLoggingSimJob, Q> jobFactory
             = new DefaultVisitsLoggingSimJobFactory<> ();
@@ -64,7 +64,7 @@ public class DefaultSimQueueTests
           for (final DefaultVisitsLoggingSimJob j : jobs) 
             actualJobQueueVisitLogs.put (j, j.getVisitLogs ());
           assert predictor.matchVisitLogs_SQ_SV
-            (queue, predictedJobQueueVisitLogs, actualJobQueueVisitLogs, DefaultSimQueueTests.ACCURACY, System.err);
+            (queue, predictedJobQueueVisitLogs, actualJobQueueVisitLogs, accuracy, System.err);
           el.reset ();
         }
     return true;
