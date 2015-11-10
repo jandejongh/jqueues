8631ce7e131c076e4d37f06af9c83613efdf8d25
Jan de Jongh <jfcmdejongh@gmail.com>
2015-11-10 12:53:18 +0000

    Added DROP tests. [PASSED 001-004].
    git-svn-id: https://jandejongh.dnsdojo.org:3690/jqueues/trunk@326 23c8eed1-2f9f-4d5d-b507-dd3313c27e4c

---
+++ java/src/nl/jdj/jqueues/r4/util/predictor/queues/SimQueuePredictor_DROP.java
@@ -0,0 +1,136 @@
+package nl.jdj.jqueues.r4.util.predictor.queues;
+
+import java.util.HashSet;
+import java.util.Set;
+import nl.jdj.jqueues.r4.SimJob;
+import nl.jdj.jqueues.r4.event.simple.SimEntitySimpleEventType;
+import nl.jdj.jqueues.r4.event.simple.SimQueueSimpleEventType;
+import nl.jdj.jqueues.r4.serverless.DROP;
+import nl.jdj.jqueues.r4.util.jobfactory.JobQueueVisitLog;
+import nl.jdj.jqueues.r4.util.predictor.AbstractSimQueuePredictor;
+import nl.jdj.jqueues.r4.util.predictor.SimQueuePredictionAmbiguityException;
+import nl.jdj.jqueues.r4.util.predictor.SimQueuePredictionException;
+import nl.jdj.jqueues.r4.util.predictor.SimQueuePredictor;
+import nl.jdj.jqueues.r4.util.predictor.state.SimQueueState;
+import nl.jdj.jqueues.r4.util.predictor.workload.WorkloadScheduleException;
+import nl.jdj.jqueues.r4.util.predictor.workload.WorkloadSchedule_SQ_SV_ROEL_U;
+
+/** A {@link SimQueuePredictor} for {@link DROP}.
+ *
+ * @param <J> The type of {@link SimJob}s supported.
+ * 
+ */
+public class SimQueuePredictor_DROP<J extends SimJob>
+extends AbstractSimQueuePredictor<J, DROP>
+{
+
+  /**
+   * 
+   * @throws IllegalStateException We do not expect queue events.
+   * 
+   */
+  @Override
+  protected boolean is_ROEL_U_UnderWorkloadQueueEventClashes (final DROP queue)
+  {
+    throw new IllegalStateException ();
+  }
+
+  @Override
+  protected double getNextQueueEventTimeBeyond
+  (final DROP queue,
+   final SimQueueState<J, DROP> queueState,
+   final Set<SimEntitySimpleEventType.Member> queueEventTypes)
+  {
+    if ( queue == null
+      || queueState == null
+      || queueEventTypes == null)
+      throw new IllegalArgumentException ();
+    queueEventTypes.clear ();
+    return Double.NaN;
+  }
+
+  @Override
+  protected void doWorkloadEvents_SQ_SV_ROEL_U
+  (final DROP queue,
+   final WorkloadSchedule_SQ_SV_ROEL_U<J, DROP> workloadSchedule,
+   final SimQueueState<J, DROP> queueState,
+   final Set<SimEntitySimpleEventType.Member> workloadEventTypes,
+   final Set<JobQueueVisitLog<J, DROP>> visitLogsSet)
+   throws SimQueuePredictionException, WorkloadScheduleException
+  {
+    if ( queue == null
+      || workloadSchedule == null
+      || queueState == null
+      || workloadEventTypes == null
+      || visitLogsSet == null)
+      throw new IllegalArgumentException ();
+    if (workloadEventTypes.size () > 1)
+      throw new SimQueuePredictionAmbiguityException ();
+    final double time = queueState.getTime ();
+    if (Double.isNaN (time))
+      throw new IllegalStateException ();
+    final SimEntitySimpleEventType.Member eventType = (workloadEventTypes.isEmpty ()
+      ? null
+      : workloadEventTypes.iterator ().next ());
+    if (eventType == null)
+    {
+      /* NOTHING (LEFT) TO DO */
+    }
+    else if (eventType == SimQueueSimpleEventType.QUEUE_ACCESS_VACATION)
+    {
+      final boolean queueAccessVacation = workloadSchedule.getQueueAccessVacationMap_SQ_SV_ROEL_U ().get (time);
+      if (queueAccessVacation)
+        queueState.startQueueAccessVacation (time);
+      else
+        queueState.stopQueueAccessVacation (time);
+    }
+    else if (eventType == SimEntitySimpleEventType.ARRIVAL)
+    {
+      final J job = workloadSchedule.getJobArrivalsMap_SQ_SV_ROEL_U ().get (time);
+      final Set<J> drops = new HashSet<> ();
+      drops.add (job);
+      queueState.doExits (time, drops, null, null, null, visitLogsSet);
+    }
+    else if (eventType == SimEntitySimpleEventType.REVOCATION)
+    {
+    }
+    else if (eventType == SimQueueSimpleEventType.SERVER_ACCESS_CREDITS)
+    {
+      final int newSac = workloadSchedule.getServerAccessCreditsMap_SQ_SV_ROEL_U ().get (time);
+      queueState.setServerAccessCredits (time, newSac);
+    }
+    else
+      throw new RuntimeException ();
+    if (eventType != null)
+      workloadEventTypes.remove (eventType);
+  }
+
+  @Override
+  protected void doQueueEvents_SQ_SV_ROEL_U
+  (final DROP queue,
+   final SimQueueState<J, DROP> queueState,
+   final Set<SimEntitySimpleEventType.Member> queueEventTypes,
+   final Set<JobQueueVisitLog<J, DROP>> visitLogsSet)
+   throws SimQueuePredictionException    
+  {
+    if ( queue == null
+      || queueState == null
+      || queueEventTypes == null
+      || visitLogsSet == null)
+      throw new IllegalArgumentException ();
+    if (queueEventTypes.size () > 1)
+      throw new SimQueuePredictionAmbiguityException ();
+    throw new IllegalStateException ();
+  }  
+  
+  @Override
+  protected void updateToTime (final DROP queue, final SimQueueState queueState, final double newTime)
+  {
+    if (queue == null || queueState == null)
+      throw new IllegalArgumentException ();
+    if (Double.isNaN (newTime))
+      throw new IllegalArgumentException ();
+    queueState.setTime (newTime);
+  }
+
+}
---
+++ java/test/nl/jdj/jqueues/r4/serverless/DROPTest.java
@@ -0,0 +1,63 @@
+package nl.jdj.jqueues.r4.serverless;
+
+import nl.jdj.jqueues.r4.DefaultSimQueueTests;
+import nl.jdj.jqueues.r4.util.jobfactory.DefaultVisitsLoggingSimJob;
+import nl.jdj.jqueues.r4.util.predictor.SimQueuePredictionException;
+import nl.jdj.jqueues.r4.util.predictor.SimQueuePredictor;
+import nl.jdj.jqueues.r4.util.predictor.queues.SimQueuePredictor_DROP;
+import nl.jdj.jsimulation.r4.SimEvent;
+import nl.jdj.jsimulation.r4.SimEventList;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ *
+ */
+public class DROPTest
+{
+  
+  public DROPTest ()
+  {
+  }
+  
+  @BeforeClass
+  public static void setUpClass ()
+  {
+  }
+  
+  @AfterClass
+  public static void tearDownClass ()
+  {
+  }
+  
+  @Before
+  public void setUp ()
+  {
+  }
+  
+  @After
+  public void tearDown ()
+  {
+  }
+
+  /**
+   * Test of DROP.
+   * 
+   */
+  @Test
+  public void testDROP () throws SimQueuePredictionException
+  {
+    final SimEventList eventList = new SimEventList (SimEvent.class);
+    final DROP queue = new DROP (eventList);
+    final SimQueuePredictor<DefaultVisitsLoggingSimJob, DROP> predictor = new SimQueuePredictor_DROP<> ();
+    final int numberOfJobs = 50;
+    final boolean silent = true;
+    final boolean deadSilent = true;
+    DefaultSimQueueTests.doSimQueueTests_SQ_SV (queue, predictor, numberOfJobs, silent, deadSilent, 1.0e-12, null);
+  }
+
+}
