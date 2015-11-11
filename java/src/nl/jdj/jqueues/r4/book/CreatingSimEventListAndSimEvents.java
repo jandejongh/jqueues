5527c9d093ad4f7b3d7a4a6b23f579fd68bee66e
Jan de Jongh <jfcmdejongh@gmail.com>
2015-11-11 22:54:51 +0000

    Added package for book examples and code extracts. Classes should be package-private as to exclude them from the javadoc.
    git-svn-id: https://jandejongh.dnsdojo.org:3690/jqueues/trunk@349 23c8eed1-2f9f-4d5d-b507-dd3313c27e4c

---
+++ java/src/nl/jdj/jqueues/r4/book/CreatingSimEventListAndSimEvents.java
@@ -0,0 +1,19 @@
+package nl.jdj.jqueues.r4.book;
+
+import nl.jdj.jsimulation.r4.SimEvent;
+import nl.jdj.jsimulation.r4.SimEventList;
+
+final class CreatingSimEventListAndSimEvents
+{
+  
+  public static void main (final String[] args)
+  {
+    final SimEventList el = new SimEventList ();
+    final SimEvent e1 = new SimEvent (5.0);
+    final SimEvent e2 = new SimEvent (10.0);
+    el.add (e1);
+    el.add (e2);
+    el.print ();
+  }
+  
+}
