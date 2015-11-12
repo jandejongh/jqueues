9fa2323653f29c1162c67abf4af4bb0c23886111
Jan de Jongh <jfcmdejongh@gmail.com>
2015-11-12 19:55:57 +0000

    Moved test files for serverless to new package.
    git-svn-id: https://jandejongh.dnsdojo.org:3690/jqueues/trunk@386 23c8eed1-2f9f-4d5d-b507-dd3313c27e4c

---
+++ java/test/nl/jdj/jqueues/r4/queues/serverless/DELAYTest.java
@@ -0,0 +1,68 @@
+package nl.jdj.jqueues.r4.queues.serverless;
+
+import nl.jdj.jqueues.r4.queue.serverless.DELAY;
+import nl.jdj.jqueues.r4.DefaultSimQueueTests;
+import nl.jdj.jqueues.r4.util.jobfactory.DefaultVisitsLoggingSimJob;
+import nl.jdj.jqueues.r4.util.predictor.SimQueuePredictionException;
+import nl.jdj.jqueues.r4.util.predictor.SimQueuePredictor;
+import nl.jdj.jqueues.r4.util.predictor.queues.SimQueuePredictor_DELAY;
+import nl.jdj.jsimulation.r4.SimEvent;
+import nl.jdj.jsimulation.r4.SimEventList;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ *
+ */
+public class DELAYTest
+{
+  
+  public DELAYTest ()
+  {
+  }
+  
+  @BeforeClass
+  public static void setUpClass ()
+  {
+  }
+  
+  @AfterClass
+  public static void tearDownClass ()
+  {
+  }
+  
+  @Before
+  public void setUp ()
+  {
+  }
+  
+  @After
+  public void tearDown ()
+  {
+  }
+
+  /**
+   * Test of DELAY.
+   * 
+   */
+  @Test
+  public void testDELAY () throws SimQueuePredictionException
+  {
+    final SimEventList eventList = new SimEventList (SimEvent.class);
+    final double[] waitingTimeValues = { 0.0, 3.39, 27.833 };
+    for (final double waitingTime : waitingTimeValues)
+    {
+      final DELAY queue = new DELAY (eventList, waitingTime);
+      final SimQueuePredictor<DefaultVisitsLoggingSimJob, DELAY> predictor = new SimQueuePredictor_DELAY<> ();
+      final int numberOfJobs = 50;
+      final boolean silent = true;
+      final boolean deadSilent = true;
+      DefaultSimQueueTests.doSimQueueTests_SQ_SV (queue, predictor, numberOfJobs, silent, deadSilent, 1.0e-12, null);
+    }
+  }
+
+}
