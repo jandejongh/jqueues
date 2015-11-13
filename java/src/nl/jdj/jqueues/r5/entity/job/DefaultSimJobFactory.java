63a416df3bb937f56b2a91fbd87a8d2d398eb1b7
Jan de Jongh <jfcmdejongh@gmail.com>
2015-11-13 01:27:25 +0000

    Migration to r5.
    git-svn-id: https://jandejongh.dnsdojo.org:3690/jqueues/trunk@399 23c8eed1-2f9f-4d5d-b507-dd3313c27e4c

---
+++ java/src/nl/jdj/jqueues/r5/entity/job/DefaultSimJobFactory.java
@@ -0,0 +1,32 @@
+package nl.jdj.jqueues.r5.entity.job;
+
+import java.util.Map;
+import nl.jdj.jqueues.r5.SimQueue;
+import nl.jdj.jqueues.r5.SimJobFactory;
+import nl.jdj.jsimulation.r4.SimEventList;
+
+/** A factory for {@link DefaultSimJob}s.
+ *
+ * @param <Q>  The queue type for jobs.
+ * 
+ * @see DefaultSimJob
+ * 
+ */
+public class DefaultSimJobFactory<Q extends SimQueue>
+implements SimJobFactory<DefaultSimJob, Q>
+{
+
+  /** Returns a new {@link DefaultSimJob} with given parameters.
+   * 
+   * @return A new {@link DefaultSimJob} with given parameters.
+   * 
+   * @see DefaultSimJob#DefaultSimJob
+   * 
+   */
+  @Override
+  public DefaultSimJob newInstance (final SimEventList eventList, final String name, final Map<Q, Double> requestedServiceTimeMap)
+  {
+    return new DefaultSimJob (eventList, name, requestedServiceTimeMap);
+  }
+  
+}
