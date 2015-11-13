e9e0035a0635c23d09c7054aadbf0e275662eb4e
Jan de Jongh <jfcmdejongh@gmail.com>
2015-11-13 16:25:02 +0000

    Migration to r5.
    git-svn-id: https://jandejongh.dnsdojo.org:3690/jqueues/trunk@417 23c8eed1-2f9f-4d5d-b507-dd3313c27e4c

---
+++ java/src/nl/jdj/jqueues/r5/misc/book/CreatingSimEventActions_AnonymousInnerClasses.java
@@ -0,0 +1,33 @@
+package nl.jdj.jqueues.r5.misc.book;
+
+import nl.jdj.jsimulation.r4.SimEvent;
+import nl.jdj.jsimulation.r4.SimEventAction;
+import nl.jdj.jsimulation.r4.SimEventList;
+
+final class CreatingSimEventActions_AnonymousInnerClasses
+{
+  
+  public static void main (final String[] args)
+  {
+    final SimEventList el = new SimEventList ();
+    final SimEvent e =
+      new SimEvent ("My First Real Event", 5.0, null, new SimEventAction ()
+      {
+        @Override
+        public final void action (final SimEvent event)
+        {
+          System.out.println ("Event=" + event + ", time=" + event.getTime () + ".");
+        }
+        @Override
+        public String toString ()
+        {
+          return "My First Action";
+        }
+      });
+    el.add (e);
+    el.print ();
+    el.run ();
+    el.print ();
+  }
+  
+}
