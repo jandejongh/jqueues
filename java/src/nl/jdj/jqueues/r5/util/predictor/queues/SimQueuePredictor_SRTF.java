1169540b82137c19d1e459d4361c841551f366d8
Jan de Jongh <jfcmdejongh@gmail.com>
2015-11-19 22:45:12 +0000

    Split of part of SimQueuePredictor_SRTF into abstract SimQueuePredictor_Preemptive.
    git-svn-id: https://jandejongh.dnsdojo.org:3690/jqueues/trunk@460 23c8eed1-2f9f-4d5d-b507-dd3313c27e4c

--- java/src/nl/jdj/jqueues/r5/util/predictor/queues/SimQueuePredictor_SRTF.java
+++ java/src/nl/jdj/jqueues/r5/util/predictor/queues/SimQueuePredictor_SRTF.java
@@ -13,7 +13,6 @@ import nl.jdj.jqueues.r5.entity.job.visitslogging.JobQueueVisitLog;
 import nl.jdj.jqueues.r5.entity.queue.preemptive.SRTF;
 import nl.jdj.jqueues.r5.event.simple.SimEntitySimpleEventType;
 import nl.jdj.jqueues.r5.event.simple.SimQueueSimpleEventType;
-import nl.jdj.jqueues.r5.util.predictor.AbstractSimQueuePredictor;
 import nl.jdj.jqueues.r5.util.predictor.SimQueuePredictionAmbiguityException;
 import nl.jdj.jqueues.r5.util.predictor.SimQueuePredictionException;
 import nl.jdj.jqueues.r5.util.predictor.SimQueuePredictor;
@@ -25,7 +24,7 @@ import nl.jdj.jqueues.r5.util.predictor.workload.WorkloadSchedule_SQ_SV_ROEL_U;
  * 
  */
 public class SimQueuePredictor_SRTF
-extends AbstractSimQueuePredictor<SRTF>
+extends SimQueuePredictor_Preemptive<SRTF>
 {
 
   @Override
@@ -51,76 +50,6 @@ extends AbstractSimQueuePredictor<SRTF>
     return time + smallestRs;
   }
 
-  protected void preemptJob
-  (final SRTF queue,
-   final SimQueueState<SimJob, SRTF> queueState,
-   final SimJob executingJob,
-   final Set<JobQueueVisitLog<SimJob, SRTF>> visitLogsSet)
-   throws SimQueuePredictionException
-  {
-    final double time = queueState.getTime ();
-    final Map<SimJob, Double> rsTimeMap = queueState.getJobRemainingServiceTimeMap ();
-    final NavigableMap<Double,List<SimJob>> rsMap = queueState.getRemainingServiceMap ();
-    switch (queue.getPreemptionStrategy ())
-    {
-      case DROP:
-        final Set<SimJob> drops = new HashSet<> ();
-        final SimJob droppedJob = executingJob;
-        drops.add (droppedJob);
-        queueState.doExits (time, drops, null, null, null, visitLogsSet);
-        break;
-      case RESUME:
-        // Nothing to do.
-        break;
-      case RESTART:
-        final double oldRs = rsTimeMap.get (executingJob);
-              // RESTART: Must always take the service time from the job!
-        // final double newRs = ((DefaultSimQueueState) queueState).getServiceTime (queue, executingJob);
-        final double newRs = executingJob.getServiceTime (queue);
-        rsTimeMap.put (executingJob, newRs);
-        rsMap.get (oldRs).remove (executingJob);
-        if (rsMap.get (oldRs).isEmpty ())
-          rsMap.remove (oldRs);
-        if (rsMap.containsKey (newRs))
-        {
-          // Start time of executing job.
-          final double startTime = queueState.getStartTimesMap ().get (executingJob);
-          // Jobs with equal remaining service time, ordered increasing in start time.
-          final List<SimJob> equalRsJobs = rsMap.get (newRs);
-          int indexToInsert = 0;
-          for (final SimJob equalRsJob: equalRsJobs)
-          {
-            final double equalRsJobStartTime = queueState.getStartTimesMap ().get (equalRsJob);
-            if (equalRsJobStartTime < startTime)
-              indexToInsert++;
-            else if (equalRsJobStartTime == startTime)
-              throw new SimQueuePredictionAmbiguityException ();
-            else
-              break;
-          }
-          rsMap.get (newRs).add (indexToInsert, executingJob);
-        }
-        else
-        {
-          rsMap.put (newRs, new ArrayList<> ());
-          rsMap.get (newRs).add (executingJob);
-        }
-        throw new UnsupportedOperationException ();
-      case REDRAW:
-        break;
-      case DEPART:
-        final Set<SimJob> departures = new HashSet<> ();
-        final SimJob departingJob = executingJob;
-        departures.add (departingJob);
-        queueState.doExits (time, null, null, departures, null, visitLogsSet);
-        break;
-      case CUSTOM:
-        throw new UnsupportedOperationException ();
-      default:
-        throw new UnsupportedOperationException ();
-    }
-  }
-  
   @Override
   protected void doWorkloadEvents_SQ_SV_ROEL_U
   (final SRTF queue,
