48742e40a752d559f1d7ecd03ec603669e96aa0a
Jan de Jongh <jfcmdejongh@gmail.com>
2016-07-12 00:55:43 +0000

    Test for CUPS. [PARTIAL COMMIT; BREAKS EXISTING CODE!]
    git-svn-id: https://jandejongh.dnsdojo.org:3690/jqueues/trunk@552 23c8eed1-2f9f-4d5d-b507-dd3313c27e4c

---
+++ java/test/nl/jdj/jqueues/r5/entity/queue/processorsharing/CUPSTest.java
@@ -0,0 +1,63 @@
+package nl.jdj.jqueues.r5.entity.queue.processorsharing;
+
+import nl.jdj.jqueues.r5.entity.queue.DefaultSimQueueTests;
+import nl.jdj.jqueues.r5.util.predictor.SimQueuePredictionException;
+import nl.jdj.jqueues.r5.util.predictor.SimQueuePredictor;
+import nl.jdj.jqueues.r5.util.predictor.queues.SimQueuePredictor_CUPS;
+import nl.jdj.jsimulation.r5.DefaultSimEventList;
+import nl.jdj.jsimulation.r5.DefaultSimEvent;
+import nl.jdj.jsimulation.r5.SimEventList;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ *
+ */
+public class CUPSTest
+{
+  
+  public CUPSTest ()
+  {
+  }
+  
+  @BeforeClass
+  public static void setUpClass ()
+  {
+  }
+  
+  @AfterClass
+  public static void tearDownClass ()
+  {
+  }
+  
+  @Before
+  public void setUp ()
+  {
+  }
+  
+  @After
+  public void tearDown ()
+  {
+  }
+
+  /**
+   * Test of CUPS.
+   * 
+   */
+  @Test
+  public void testCUPS () throws SimQueuePredictionException
+  {
+    final SimEventList eventList = new DefaultSimEventList (DefaultSimEvent.class);
+    final CUPS queue = new CUPS (eventList);
+    final SimQueuePredictor<CUPS> predictor = new SimQueuePredictor_CUPS ();
+    final int numberOfJobs = 100;
+    final boolean silent = true;
+    final boolean deadSilent = true;
+    DefaultSimQueueTests.doSimQueueTests_SQ_SV (queue, predictor, numberOfJobs, silent, deadSilent, 1.0e-6, null);
+  }
+
+}
