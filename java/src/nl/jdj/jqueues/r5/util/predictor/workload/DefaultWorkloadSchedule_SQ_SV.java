03d5464feaa1c77a1f784ebb5939b4f051539764
Jan de Jongh <jfcmdejongh@gmail.com>
2016-07-15 23:05:45 +0000

    Improved support for IOEL instantiations. Comments + javadoc. Improved generic support for event types. Inserted DefaultWorkloadSchedule_SQ_SV into chain for direct IOEL support.
    git-svn-id: https://jandejongh.dnsdojo.org:3690/jqueues/trunk@576 23c8eed1-2f9f-4d5d-b507-dd3313c27e4c

--- java/src/nl/jdj/jqueues/r5/util/predictor/workload/DefaultWorkloadSchedule_SQ_SV.java
+++ java/src/nl/jdj/jqueues/r5/util/predictor/workload/DefaultWorkloadSchedule_SQ_SV.java
@@ -1,16 +1,17 @@
 package nl.jdj.jqueues.r5.util.predictor.workload;
 
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import nl.jdj.jqueues.r5.SimQueue;
 import nl.jdj.jqueues.r5.event.SimEntityEvent;
 
-/** A default implementation of {@link WorkloadSchedule_SQ_SV_ROEL_U}.
+/** A default implementation of {@link WorkloadSchedule_SQ_SV}.
  *
  */
-public class DefaultWorkloadSchedule_SQ_SV_ROEL_U
-extends DefaultWorkloadSchedule_SQ_SV
-implements WorkloadSchedule_SQ_SV_ROEL_U
+public class DefaultWorkloadSchedule_SQ_SV
+extends DefaultWorkloadSchedule
+implements WorkloadSchedule_SQ_SV
 {
   
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -19,59 +20,65 @@ implements WorkloadSchedule_SQ_SV_ROEL_U
   //
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   
-  /** Creates a new {@link DefaultWorkloadSchedule_SQ_SV_ROEL_U}, filling out all the internal sets and maps from scanning a set of
+  private static
+  Set<SimQueue>
+  getQueueSet
+  (final SimQueue queue)
+  {
+    if (queue == null)
+      throw new IllegalArgumentException ();
+    final Set<SimQueue> allQueues = new HashSet<> ();
+    allQueues.add (queue);
+    return allQueues;
+  }
+    
+  /** Creates a new {@link DefaultWorkloadSchedule_SQ_SV}, filling out all the internal sets and maps from scanning a set of
    *  {@link SimEntityEvent}s for a single specified queue, single visits to that queue for each job, and an unambiguous schedule
-   *  on a Random-Order Event List.
+   *  represented as a sequence of events.
    * 
    * @param <E>         The event type.
    * @param queue       The queue to consider, non-{@code null}; events related to other queues are ignored.
    * @param queueEvents The set of events to parse (parsing is actually done in this constructor).
    * 
    * @throws IllegalArgumentException           If the queue is {@code null}.
-   * @throws WorkloadScheduleAmbiguityException If the workload represented by the {@code queueEvents} argument is
-   *                                            ambiguous on a Random-Order Event List.
    * @throws WorkloadScheduleInvalidException   If the workload represented by the {@code queueEvents} argument is invalid
    *                                            (e.g., containing a job with multiple visits to the {@code queue}.
    * 
-   * @see #isUnambiguous_ROEL
-   * 
    */
   public <E extends SimEntityEvent>
-  DefaultWorkloadSchedule_SQ_SV_ROEL_U
+  DefaultWorkloadSchedule_SQ_SV
   (final SimQueue queue,
    final Set<E> queueEvents)
   throws WorkloadScheduleException
   {
-    super (queue, queueEvents);
-    if (! isUnambiguous_ROEL ())
-      throw new WorkloadScheduleAmbiguityException ();
+    super (getQueueSet (queue), queueEvents);
+    if (! (isSingleQueue () && isSingleVisit ()))
+      throw new WorkloadScheduleInvalidException ();
   }
   
   /** Creates a new {@link DefaultWorkloadSchedule_SQ_SV}, filling out all the internal sets and maps from scanning a map of 
    *  event time onto sets of {@link SimEntityEvent}s
    *  for a single specified queue, single visits to that queue for each job, and an unambiguous schedule
-   *  on a Random-Order Event List.
+   *  represented as a sequence of events.
    * 
    * @param <E>         The event type.
    * @param queue       The queue to consider, non-{@code null}; events related to other queues are ignored.
    * @param queueEvents The set of events to parse (parsing is actually done in this constructor).
    * 
    * @throws IllegalArgumentException           If the queue is {@code null}.
-   * @throws WorkloadScheduleAmbiguityException If the workload represented by the {@code queueEvents} argument is
-   *                                            ambiguous on a Random-Order Event List.
    * @throws WorkloadScheduleInvalidException   If the workload represented by the {@code queueEvents} argument is invalid
    *                                            (e.g., containing a job with multiple visits to the {@code queue}.
    * 
    */
   public <E extends SimEntityEvent>
-  DefaultWorkloadSchedule_SQ_SV_ROEL_U
+  DefaultWorkloadSchedule_SQ_SV
   (final SimQueue queue,
    final Map<Double, Set<E>> queueEvents)
   throws WorkloadScheduleException
   {
-    super (queue, queueEvents);
-    if (! isUnambiguous_ROEL ())
-      throw new WorkloadScheduleAmbiguityException ();    
+    super (getQueueSet (queue), queueEvents);
+    if (! (isSingleQueue () && isSingleVisit ()))
+      throw new WorkloadScheduleInvalidException ();
   }
   
 }
