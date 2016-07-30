97c3551b3afcf5fb9ba4d8b67f9d75d9cd072f02
Jan de Jongh <jfcmdejongh@gmail.com>
2016-07-30 20:02:41 +0000

    [PARTIAL COMMIT; DOES NOT COMPILE!!] Added GATE_OPEN and GATE_CLOSED members.
    git-svn-id: https://jandejongh.dnsdojo.org:3690/jqueues/trunk@662 23c8eed1-2f9f-4d5d-b507-dd3313c27e4c

--- java/src/nl/jdj/jqueues/r5/extensions/gate/SimQueueWithGateSimpleEventType.java
+++ java/src/nl/jdj/jqueues/r5/extensions/gate/SimQueueWithGateSimpleEventType.java
@@ -17,4 +17,14 @@ extends SimQueueSimpleEventType
    */
   public static Member GATE = new Member ("GATE");
   
+  /** Notification that the gate is open.
+   * 
+   */
+  public static Member GATE_OPEN = new Member ("GATE_OPEN");
+  
+  /** Notification that the gate is closed.
+   * 
+   */
+  public static Member GATE_CLOSED = new Member ("GATE_CLOSED");
+  
 }
