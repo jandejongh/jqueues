82cd35c7343f0d5c54ddcff3e690a80888ea9ef2
Jan de Jongh <jfcmdejongh@gmail.com>
2016-07-30 20:05:35 +0000

    [PARTIAL COMMIT; DOES NOT COMPILE!!] Added QAV_START/QAV_END notifications. Added OUT_OF_SAC/REGAINED_SAC notifications. Added NWA_FALSE/NWA_TRUE notifications.
    git-svn-id: https://jandejongh.dnsdojo.org:3690/jqueues/trunk@664 23c8eed1-2f9f-4d5d-b507-dd3313c27e4c

--- java/src/nl/jdj/jqueues/r5/event/simple/SimQueueSimpleEventType.java
+++ java/src/nl/jdj/jqueues/r5/event/simple/SimQueueSimpleEventType.java
@@ -1,11 +1,11 @@
 package nl.jdj.jqueues.r5.event.simple;
 
-import nl.jdj.jqueues.r5.SimJob;
+import nl.jdj.jqueues.r5.SimQueue;
 import nl.jdj.jqueues.r5.event.SimEntityEvent;
 import nl.jdj.jqueues.r5.event.SimQueueAccessVacationEvent;
 import nl.jdj.jqueues.r5.event.SimQueueServerAccessCreditsEvent;
 
-/** A simple representation of a {@link SimEntityEvent} specific to {@link SimJob}s.
+/** A simple representation of a {@link SimEntityEvent} specific to {@link SimQueue}s.
  * 
  */
 public interface SimQueueSimpleEventType
@@ -19,11 +19,49 @@ extends SimEntitySimpleEventType
    */
   public static Member QUEUE_ACCESS_VACATION = new Member ("QUEUE_ACCESS_VACATION");
   
+  /** The start of a queue-access vacation.
+   * 
+   * @see SimQueueAccessVacationEvent
+   * 
+   */
+  public static Member QAV_START = new Member ("QAV_START");
+  
+  /** The end of a queue-access vacation.
+   * 
+   * @see SimQueueAccessVacationEvent
+   * 
+   */
+  public static Member QAV_END = new Member ("QAV_END");
+  
   /** The setting of the remaining server-access credits.
    * 
    * @see SimQueueServerAccessCreditsEvent
    * 
    */
   public static Member SERVER_ACCESS_CREDITS = new Member ("SERVER_ACCESS_CREDITS");
+  
+  /** The loss of remaining server-access credits.
+   * 
+   * @see SimQueueServerAccessCreditsEvent
+   * 
+   */
+  public static Member OUT_OF_SAC = new Member ("OUT_OF_SAC");
+  
+  /** The regain of remaining server-access credits.
+   * 
+   * @see SimQueueServerAccessCreditsEvent
+   * 
+   */
+  public static Member REGAINED_SAC = new Member ("REGAIN_SAC");
+  
+  /** The loss of {@code noWaitArmed}.
+   * 
+   */
+  public static Member NWA_FALSE = new Member ("NWA_FALSE");
+  
+  /** The gain of {@code noWaitArmed}.
+   * 
+   */
+  public static Member NWA_TRUE = new Member ("NWA_TRUE");
   
 }
