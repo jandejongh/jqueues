0d0d6f87afdd3519739980daca015ceddfb80696
Jan de Jongh <jfcmdejongh@gmail.com>
2016-08-10 14:26:45 +0000

    Added LeakyBucket[0.0] == SINK. Added LeakyBucket[Infinity] == ZERO. [CLEAN COMMIT]
    git-svn-id: https://jandejongh.dnsdojo.org:3690/jqueues/trunk@758 23c8eed1-2f9f-4d5d-b507-dd3313c27e4c

--- java/test/nl/jdj/jqueues/r5/entity/queue/serverless/LeakyBucketTest.java
+++ java/test/nl/jdj/jqueues/r5/entity/queue/serverless/LeakyBucketTest.java
@@ -52,16 +52,24 @@ public class LeakyBucketTest
   public void testLeakyBucket () throws SimQueuePredictionException
   {
     final SimEventList eventList = new DefaultSimEventList (DefaultSimEvent.class);
+    final int numberOfJobs = 50;
+    final boolean silent = true;
+    final boolean deadSilent = true;
     final double[] rateLimitValues = { 0.0, 0.001, 0.1, 0.5, 2.0, 10.0, Double.POSITIVE_INFINITY };
     for (final double rateLimit : rateLimitValues)
     {
       final LeakyBucket queue = new LeakyBucket (eventList, rateLimit);
       final SimQueuePredictor predictor = new SimQueuePredictor_LeakyBucket ();
-      final int numberOfJobs = 50;
-      final boolean silent = true;
-      final boolean deadSilent = true;
       DefaultSimQueueTests.doSimQueueTests_SQ_SV (queue, predictor, null, numberOfJobs, null, silent, deadSilent, 1.0e-12, null);
     }
+    // LeakyBucket[0.0] == SINK
+    final LeakyBucket lb_zero = new LeakyBucket<> (eventList, 0);
+    final SINK sink = new SINK<> (eventList);
+    DefaultSimQueueTests.doSimQueueTests_SQ_SV (lb_zero, null, sink, numberOfJobs, null, silent, deadSilent, 1.0e-12, null);
+    // LeakyBucket[infinity] == ZERO
+    final LeakyBucket lb_inf = new LeakyBucket<> (eventList, Double.POSITIVE_INFINITY);
+    final ZERO zero = new ZERO<> (eventList);
+    DefaultSimQueueTests.doSimQueueTests_SQ_SV (lb_inf, null, zero, numberOfJobs, null, silent, deadSilent, 1.0e-12, null);
   }
 
 }
