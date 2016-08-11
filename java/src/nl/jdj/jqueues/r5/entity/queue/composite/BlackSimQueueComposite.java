8214f7e7339b1aacd2ef41f1ec3421c22e823a6c
Jan de Jongh <jfcmdejongh@gmail.com>
2016-08-11 16:29:05 +0000

    NoWaitArmed -> StartArmed noWaitArmed -> startArmed NWA -> STA Formitting, doc and javadoc. [PARTIAL COMMIT]
    git-svn-id: https://jandejongh.dnsdojo.org:3690/jqueues/trunk@776 23c8eed1-2f9f-4d5d-b507-dd3313c27e4c

--- java/src/nl/jdj/jqueues/r5/entity/queue/composite/BlackSimQueueComposite.java
+++ java/src/nl/jdj/jqueues/r5/entity/queue/composite/BlackSimQueueComposite.java
@@ -4,7 +4,6 @@ import nl.jdj.jqueues.r5.SimJob;
 import nl.jdj.jqueues.r5.SimQueue;
 import nl.jdj.jqueues.r5.entity.queue.composite.dual.ctandem2.BlackCompressedTandem2SimQueue;
 import nl.jdj.jqueues.r5.entity.queue.composite.single.enc.BlackEncapsulatorSimQueue;
-import nl.jdj.jqueues.r5.entity.queue.serverless.DELAY;
 
 /** A network of {@link SimQueue}s embedded in a single queue hiding its internal structural details.
  *
@@ -62,8 +61,7 @@ extends SimQueueComposite<DJ, DQ, J, Q>
      * the server-access credits on this (super) queue.
      * The super queue never sets the server-access credits on its sub-queues, and,
      * as a result, the server-access credits on all sub-queues is always infinite.
-     * The {@link #isNoWaitArmed} state is always {@code true} on this queue, because (real) jobs start immediately
-     * in absence of queue-access vacations and out-of-service-access-credits conditions.
+     * The {@link #isStartArmed} state is always {@code true} on this queue.
      * 
      */
     LOCAL,
@@ -75,14 +73,15 @@ extends SimQueueComposite<DJ, DQ, J, Q>
      * Whenever a real job is in the waiting area of the (super) queue, so is its delegate job in the sub-queue.
      * The same holds for the service area.
      * The number of server access credits on this queue and on the sub-queue are always equal,
-     * and so are the {@link #isNoWaitArmed} states.
+     * and so are the {@link #isStartArmed} states.
      * 
      * @see BlackEncapsulatorSimQueue
      * 
      */
     ENCAPSULATOR_QUEUE,
-    /** The waiting area of the first queue is that of the composite queue; the service area of the second queue is that of the
-     *  composite queue in a system with exactly two sub-queues.
+    /** The waiting area of the first queue is that of the composite queue;
+     *  the service area of the second queue is that of the composite queue
+     *  in a system with exactly two sub-queues.
      * 
      * <p>
      * This model can only be applied in case of exactly two sub-queues.
@@ -94,29 +93,13 @@ extends SimQueueComposite<DJ, DQ, J, Q>
      * independently limit the remaining number of jobs to start (as in {@link StartModel#LOCAL}.
      * The number of server access credits on the first sub-queue is always zero or one;
      * it is zero if the number of server-access credits on the super (this) queue is zero or
-     * if the server queue has {@code noWaitArmed == false}.
+     * if the server queue has {@code StartArmed == false}.
      * In all other cases, it is one.
      * In other words, the server-access credits on the wait queue serve as a binary gate for the
      * combined conditions that there are server-access credits on the super (this) queue,
-     * and that the server queue is in {@code noWaitArmed} state.
+     * and that the server queue is in {@code StartArmed} state.
      * The number of server access credits on the second sub-queue is always infinite (i.e., unused).
-     * The {@link #isNoWaitArmed} state on this queue, is the logical AND operation of the
-     * {@link #isNoWaitArmed} states of the individual sub-queues.
-     * 
-     * <p>
-     * The latter condition may come as a surprise,
-     * but in order to see its validity,
-     * note that the {@code true} {@link #isNoWaitArmed} state on the server queue does indeed guarantee that 
-     * the wait queue has server-access credits in presence of server-access credits on the super (this) queue
-     * (which are to be ignored for the determination of the local {@link #isNoWaitArmed} state).
-     * It is also clear that this is a requirement for {@link #isNoWaitArmed} on the super (this) queue.
-     * However, this in itself does <i>not</i> necessarily guarantee a {@link #isNoWaitArmed} state on the wait queue,
-     * and we clearly need this requirement as well, because an arriving job should <i>not</i> be forced to wait at the
-     * wait-queue for {@link #isNoWaitArmed} being {@code true} on the super (this) queue).
-     * It is not guaranteed in general that non-zero server-access credits on a {@link SimQueue}
-     * result in {@link #isNoWaitArmed} being {@code true}, see, e.g., {@link DELAY}.
-     * Hence we must require {@link #isNoWaitArmed} on the wait queue independently for
-     * {@link #isNoWaitArmed} on the super (this) queue.
+     * The {@link #isStartArmed} state on this queue is that of the serve (second) queue.
      * 
      * @see BlackCompressedTandem2SimQueue
      * 
