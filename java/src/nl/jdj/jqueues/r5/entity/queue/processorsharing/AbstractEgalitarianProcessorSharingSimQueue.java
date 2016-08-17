6bc5e5404e16086d677a5907a17b687518e7ec48
Jan de Jongh <jfcmdejongh@gmail.com>
2016-08-17 16:26:46 +0000

    Restructured processorsharing queues, in preparation of HOL_PS. [CLEAN COMMIT]
    git-svn-id: https://jandejongh.dnsdojo.org:3690/jqueues/trunk@814 23c8eed1-2f9f-4d5d-b507-dd3313c27e4c

---
+++ java/src/nl/jdj/jqueues/r5/entity/queue/processorsharing/AbstractEgalitarianProcessorSharingSimQueue.java
@@ -0,0 +1,313 @@
+package nl.jdj.jqueues.r5.entity.queue.processorsharing;
+
+import java.util.Set;
+import nl.jdj.jqueues.r5.SimJob;
+import nl.jdj.jqueues.r5.SimQueue;
+import nl.jdj.jqueues.r5.util.collection.HashMapWithPreImageAndOrderedValueSet;
+import nl.jdj.jsimulation.r5.SimEventList;
+
+/** An abstract base class for egalitarian processor-sharing queueing disciplines.
+ *
+ * <p>
+ * In <i>egalitarian</i> processor-sharing queueing disciplines,
+ * all jobs in the service area are served simultaneously at equal rates,
+ * until completion of one of them.
+ * The service rate, naturally, is inversely proportional to the number of jobs in the service area.
+ * 
+ * <p>
+ * The remaining degrees of freedom to sub-classes are when to start which jobs.
+ * 
+ * @param <J> The type of {@link SimJob}s supported.
+ * @param <Q> The type of {@link SimQueue}s supported.
+ * 
+ * @see PS
+ * @see HOL_PS
+ * 
+ */
+public abstract class AbstractEgalitarianProcessorSharingSimQueue
+  <J extends SimJob, Q extends AbstractEgalitarianProcessorSharingSimQueue>
+  extends AbstractProcessorSharingSimQueue<J, Q>
+  implements SimQueue<J, Q>
+{
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // CONSTRUCTOR(S) / CLONING / FACTORIES
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  /** Creates a egalitarian processor-sharing queue given an event list, buffer size and number of servers.
+   *
+   * <p>
+   * The constructor registers a pre-update hook that sets the virtual time upon updates.
+   * 
+   * @param eventList       The event list to use.
+   * @param bufferSize      The buffer size (non-negative), {@link Integer#MAX_VALUE} is interpreted as infinity.
+   * @param numberOfServers The number of servers (non-negative), {@link Integer#MAX_VALUE} is interpreted as infinity.
+   *
+   * @see #registerPreUpdateHook
+   * @see #updateVirtualTime
+   * 
+   */
+  protected AbstractEgalitarianProcessorSharingSimQueue
+  (final SimEventList eventList, final int bufferSize, final int numberOfServers)
+  {
+    super (eventList, bufferSize, numberOfServers);
+    registerPreUpdateHook (this::updateVirtualTime);
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // VIRTUAL TIME
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  /** The current virtual time; updates in {@link #update}.
+   * 
+   */
+  private double virtualTime = 0;
+  
+  /** Returns the current virtual time.
+   * 
+   * <p>
+   * The virtual time for an {@link AbstractEgalitarianProcessorSharingSimQueue}
+   * is zero when no jobs are being executed, and increases in time inversely proportional to the number of jobs in execution.
+   * Hence, if there is only one job in execution, the virtual time increases at the same rate as the ("real") time,
+   * but if {@code N > 1} jobs are being executed, the virtual time increases linearly with slope {@code 1/N} in time.
+   * 
+   * <p>
+   * The virtual time is kept consistent in {@link #update}.
+   * 
+   * <p>
+   * Note that with an {@link AbstractEgalitarianProcessorSharingSimQueue},
+   * the virtual departure time of a job can be calculated at the time the job is taken into service;
+   * it does not change afterwards
+   * (as the required service time cannot change during a queue visit, by contract of {@link SimQueue}).
+   * 
+   * @return The current virtual time.
+   * 
+   * @see #update
+   * 
+   */
+  protected final double getVirtualTime ()
+  {
+    return this.virtualTime;
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // VIRTUAL DEPARTURE TIME
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  /** The mapping from jobs in {@link #jobsInServiceArea} to their respective virtual departure times.
+   * 
+   * <p>
+   * The special extensions to <code>TreeMap</code> allow for efficient  determination of the pre-images of
+   * virtual departure times.
+   * 
+   */
+  protected final HashMapWithPreImageAndOrderedValueSet<J, Double> virtualDepartureTime
+    = new HashMapWithPreImageAndOrderedValueSet<> ();
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // RESET
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  /** Calls super method, resets the virtual time to zero and clears the virtual departure time map.
+   * 
+   */
+  @Override
+  protected void resetEntitySubClass ()
+  {
+    super.resetEntitySubClass ();
+    this.virtualTime = 0;
+    this.virtualDepartureTime.clear ();
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // UPDATE
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  /** Updates the virtual time.
+   * 
+   * <p>
+   * This method is called as an update hook, and not meant to be called from user code (in sub-classes).
+   * It is left protected for {@code javadoc}.
+   * 
+   * @param newTime The new time.
+   * 
+   * @see #getNumberOfJobsInServiceArea
+   * @see #getVirtualTime
+   * @see #getLastUpdateTime
+   * @see #registerPreUpdateHook
+   * 
+   */
+  protected final void updateVirtualTime (final double newTime)
+  {
+    if (newTime < getLastUpdateTime ())
+      throw new IllegalStateException ();
+    final int numberOfJobsExecuting = getNumberOfJobsInServiceArea ();
+    if (numberOfJobsExecuting == 0)
+      this.virtualTime = 0;
+    else if (newTime > getLastUpdateTime ())
+      this.virtualTime += ((newTime - getLastUpdateTime ()) / numberOfJobsExecuting);
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // START
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  /** Inserts the job, after sanity checks, in the service area and administers its virtual departure time.
+   * 
+   * @see #getServiceTimeForJob
+   * @see #getVirtualTime
+   * @see #virtualDepartureTime
+   * 
+   */
+  @Override
+  protected final void insertJobInQueueUponStart (final J job, final double time)
+  {
+    if (job == null
+    || (! getJobs ().contains (job))
+    || getJobsInServiceArea ().contains (job)
+    || this.virtualDepartureTime.containsKey (job))
+      throw new IllegalArgumentException ();
+    this.jobsInServiceArea.add (job);
+    final double jobServiceTime = getServiceTimeForJob (job);
+    if (jobServiceTime < 0)
+      throw new RuntimeException ();
+    final double jobVirtualDepartureTime = getVirtualTime () + jobServiceTime;
+    this.virtualDepartureTime.put (job, jobVirtualDepartureTime);
+  }
+
+  /** Reschedules due to the start of a job, making it depart immediately if its requested service time is zero,
+   *  or rescheduling the (single) departure event of this queue otherwise.
+   * 
+   * @see #virtualDepartureTime
+   * @see #rescheduleDepartureEvent
+   * @see #depart
+   * 
+   */
+  @Override
+  protected final void rescheduleAfterStart (final J job, final double time)
+  {
+    if (job == null
+    || (! getJobs ().contains (job))
+    || (! getJobsInServiceArea ().contains (job))
+    || ! this.virtualDepartureTime.containsKey (job))
+      throw new IllegalArgumentException ();
+    final double jobServiceTime = this.virtualDepartureTime.get (job);
+    if (jobServiceTime < 0)
+      throw new RuntimeException ();
+    if (jobServiceTime > 0)
+      rescheduleDepartureEvent ();
+    else
+      depart (time, job);
+  }
+    
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // SERVICE TIME FOR JOB
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  /** Calls super method (in order to make implementation final).
+   * 
+   * @return The result from the super method.
+   * 
+   */
+  @Override
+  protected final double getServiceTimeForJob (final J job)
+  {
+    return super.getServiceTimeForJob (job);
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // DEPARTURE
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  /** Calls {@link #removeJobFromQueueUponRevokation} for the departed job.
+   * 
+   * @see #removeJobFromQueueUponRevokation
+   * 
+   */
+  @Override
+  protected final void removeJobFromQueueUponDeparture (final J departingJob, final double time)
+  {
+    removeJobFromQueueUponRevokation (departingJob, time);
+  }
+
+  /** Calls {@link #rescheduleDepartureEvent}.
+   * 
+   * @see #rescheduleDepartureEvent
+   * 
+   */
+  @Override
+  protected final void rescheduleAfterDeparture (final J departedJob, final double time)
+  {
+    if (departedJob == null)
+      throw new IllegalArgumentException ();
+    rescheduleDepartureEvent ();
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // RESCHEDULE DEPARTURE EVENT
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  /** Reschedules the single departure event for this queue.
+   * 
+   * <p>
+   * First, cancels a pending departure event.
+   * Then, determines which job in the service area (if any) is to depart first through {@link #virtualDepartureTime},
+   * and schedules a departure event for it (or makes the job depart immediately through {@link #depart} if its
+   * remaining service time is zero.
+   * 
+   * @see #getDepartureEvents
+   * @see #cancelDepartureEvent
+   * @see #jobsInServiceArea
+   * @see #virtualDepartureTime
+   * @see #getVirtualTime
+   * @see #depart
+   * @see #scheduleDepartureEvent
+   * 
+   */
+  protected final void rescheduleDepartureEvent ()
+  {
+    final Set<DefaultDepartureEvent<J, Q>> departureEvents = getDepartureEvents ();
+    if (departureEvents == null)
+      throw new RuntimeException ();
+    if (departureEvents.size () > 1)
+      throw new IllegalStateException ();
+    if (departureEvents.size () > 0)
+      cancelDepartureEvent (departureEvents.iterator ().next ());
+    if (! this.virtualDepartureTime.isEmpty ())
+    {
+      if (getNumberOfJobsInServiceArea () == 0)
+        throw new IllegalStateException ();
+      final double scheduleVirtualTime = this.virtualDepartureTime.firstValue ();
+      final double deltaVirtualTime = scheduleVirtualTime - getVirtualTime ();
+      if (deltaVirtualTime < 0)
+        throw new IllegalStateException ();
+      final J job = this.virtualDepartureTime.getPreImageForValue (scheduleVirtualTime).iterator ().next ();
+      if (deltaVirtualTime == 0)
+        depart (getLastUpdateTime (), job);
+      else
+      {
+        final double deltaTime = deltaVirtualTime * getNumberOfJobsInServiceArea ();
+        scheduleDepartureEvent (getLastUpdateTime () + deltaTime, job);
+      }
+    }
+  }
+  
+}
--- java/src/nl/jdj/jqueues/r5/entity/queue/processorsharing/PS.java
+++ java/src/nl/jdj/jqueues/r5/entity/queue/processorsharing/PS.java
@@ -1,9 +1,7 @@
 package nl.jdj.jqueues.r5.entity.queue.processorsharing;
 
-import java.util.Set;
 import nl.jdj.jqueues.r5.SimJob;
 import nl.jdj.jqueues.r5.SimQueue;
-import nl.jdj.jqueues.r5.util.collection.HashMapWithPreImageAndOrderedValueSet;
 import nl.jdj.jsimulation.r5.SimEventList;
 
 /** The single-server {@link PS} queue serves all jobs simultaneously, equally distributing its service capacity.
@@ -13,13 +11,15 @@ import nl.jdj.jsimulation.r5.SimEventList;
  * 
  * <p>
  * The (Egalitarian) Processor-Sharing queueing system distributes its service capacity equally among the jobs in execution.
+ * Jobs start immediately upon arrival, provided that there are server-access credits left.
+ * Upon regaining server-access credits, jobs are started in arrival order.
  *
  * @param <J> The type of {@link SimJob}s supported.
  * @param <Q> The type of {@link SimQueue}s supported.
  *
  */
 public class PS<J extends SimJob, Q extends PS>
-extends AbstractProcessorSharingSimQueue<J, Q>
+extends AbstractEgalitarianProcessorSharingSimQueue<J, Q>
 {
 
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -30,19 +30,12 @@ extends AbstractProcessorSharingSimQueue<J, Q>
   
   /** Creates a single-server PS queue with infinite buffer size given an event list.
    *
-   * <p>
-   * The constructor registers a pre-update hook that sets the virtual time upon updates.
-   * 
    * @param eventList The event list to use.
    *
-   * @see #registerPreUpdateHook
-   * @see #updateVirtualTime
-   * 
    */
   public PS (final SimEventList eventList)
   {
     super (eventList, Integer.MAX_VALUE, 1);
-    registerPreUpdateHook (this::updateVirtualTime);
   }
   
   /** Returns a new {@link PS} object on the same {@link SimEventList}.
@@ -101,105 +94,19 @@ extends AbstractProcessorSharingSimQueue<J, Q>
   
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   //
-  // VIRTUAL TIME
-  //
-  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  
-  /** The current virtual time; updates in {@link #update}.
-   * 
-   */
-  private double virtualTime = 0;
-  
-  /** Returns the current virtual time.
-   * 
-   * <p>
-   * The virtual time for a {@link PS} is zero when no jobs are being executed, and increases in time inversely proportional
-   * to the number of jobs in execution.
-   * Hence, if there is only one job in execution, the virtual time increases at the same rate as the ("real") time,
-   * but if {@code N > 1} jobs are being executed, the virtual time increases linearly with slope {@code 1/N} in time.
-   * 
-   * <p>
-   * The virtual time is kept consistent in {@link #update}.
-   * 
-   * <p>
-   * Note that with {@link PS}, the virtual departure time of a job can be calculated at the time the job is taken into service;
-   * it does not change afterwards (as the required service time cannot change during a queue visit, by contract of
-   * {@link SimQueue}).
-   * 
-   * @return The current virtual time.
-   * 
-   * @see #update
-   * 
-   */
-  protected final double getVirtualTime ()
-  {
-    return this.virtualTime;
-  }
-  
-  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  //
-  // VIRTUAL DEPARTURE TIME
-  //
-  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  
-  /** The mapping from jobs in {@link #jobsInServiceArea} to their respective virtual departure times.
-   * 
-   * <p>
-   * The special extensions to <code>TreeMap</code> allow for efficient  determination of the pre-images of
-   * virtual departure times.
-   * 
-   */
-  protected final HashMapWithPreImageAndOrderedValueSet<J, Double> virtualDepartureTime
-    = new HashMapWithPreImageAndOrderedValueSet<> ();
-  
-  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  //
   // RESET
   //
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   
-  /** Calls super method, resets the virtual time to zero and clears the virtual departure time map.
+  /** Calls super method (in order to make implementation final).
    * 
    */
   @Override
   protected final void resetEntitySubClass ()
   {
     super.resetEntitySubClass ();
-    this.virtualTime = 0;
-    this.virtualDepartureTime.clear ();
   }
   
-  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  //
-  // UPDATE
-  //
-  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  
-  /** Updates the virtual time.
-   * 
-   * <p>
-   * This method is called as an update hook, and not meant to be called from user code (in sub-classes).
-   * It is left protected for {@code javadoc}.
-   * 
-   * @param newTime The new time.
-   * 
-   * @see #getNumberOfJobsInServiceArea
-   * @see #getVirtualTime
-   * @see #getLastUpdateTime
-   * @see #registerPreUpdateHook
-   * 
-   */
-  protected final void updateVirtualTime (final double newTime)
-  {
-    if (newTime < getLastUpdateTime ())
-      throw new IllegalStateException ();
-    final int numberOfJobsExecuting = getNumberOfJobsInServiceArea ();
-    if (numberOfJobsExecuting == 0)
-      this.virtualTime = 0;
-    else if (newTime > getLastUpdateTime ())
-      this.virtualTime += ((newTime - getLastUpdateTime ()) / numberOfJobsExecuting);
-  }
-
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   //
   // ARRIVAL
@@ -210,6 +117,7 @@ extends AbstractProcessorSharingSimQueue<J, Q>
    * 
    * @see #arrive
    * @see #jobQueue
+   * @see #rescheduleAfterArrival
    * 
    */
   @Override
@@ -222,6 +130,7 @@ extends AbstractProcessorSharingSimQueue<J, Q>
    * 
    * @see #hasServerAcccessCredits
    * @see #start
+   * @see #insertJobInQueueUponArrival
    * 
    */
   @Override
@@ -357,156 +266,4 @@ extends AbstractProcessorSharingSimQueue<J, Q>
     return true;
   }
 
-  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  //
-  // START
-  //
-  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  
-  /** Inserts the job, after sanity checks, in the service area and administers its virtual departure time.
-   * 
-   * @see #getServiceTimeForJob
-   * @see #getVirtualTime
-   * @see #virtualDepartureTime
-   * 
-   */
-  @Override
-  protected final void insertJobInQueueUponStart (final J job, final double time)
-  {
-    if (job == null
-    || (! getJobs ().contains (job))
-    || getJobsInServiceArea ().contains (job)
-    || this.virtualDepartureTime.containsKey (job))
-      throw new IllegalArgumentException ();
-    this.jobsInServiceArea.add (job);
-    final double jobServiceTime = getServiceTimeForJob (job);
-    if (jobServiceTime < 0)
-      throw new RuntimeException ();
-    final double jobVirtualDepartureTime = getVirtualTime () + jobServiceTime;
-    this.virtualDepartureTime.put (job, jobVirtualDepartureTime);
-  }
-
-  /** Reschedules due to the start of a job, making it depart immediately if its requested service time is zero,
-   *  or rescheduling the (single) departure event of this queue otherwise.
-   * 
-   * @see #virtualDepartureTime
-   * @see #rescheduleDepartureEvent
-   * @see #depart
-   * 
-   */
-  @Override
-  protected final void rescheduleAfterStart (final J job, final double time)
-  {
-    if (job == null
-    || (! getJobs ().contains (job))
-    || (! getJobsInServiceArea ().contains (job))
-    || ! this.virtualDepartureTime.containsKey (job))
-      throw new IllegalArgumentException ();
-    final double jobServiceTime = this.virtualDepartureTime.get (job);
-    if (jobServiceTime < 0)
-      throw new RuntimeException ();
-    if (jobServiceTime > 0)
-      rescheduleDepartureEvent ();
-    else
-      depart (time, job);
-  }
-    
-  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  //
-  // SERVICE TIME FOR JOB
-  //
-  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  
-  /** Calls super method (in order to make implementation final).
-   * 
-   * @return The result from the super method.
-   * 
-   */
-  @Override
-  protected final double getServiceTimeForJob (final J job)
-  {
-    return super.getServiceTimeForJob (job);
-  }
-  
-  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  //
-  // DEPARTURE
-  //
-  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  
-  /** Calls {@link #removeJobFromQueueUponRevokation} for the departed job.
-   * 
-   * @see #removeJobFromQueueUponRevokation
-   * 
-   */
-  @Override
-  protected final void removeJobFromQueueUponDeparture (final J departingJob, final double time)
-  {
-    removeJobFromQueueUponRevokation (departingJob, time);
-  }
-
-  /** Calls {@link #rescheduleDepartureEvent}.
-   * 
-   * @see #rescheduleDepartureEvent
-   * 
-   */
-  @Override
-  protected final void rescheduleAfterDeparture (final J departedJob, final double time)
-  {
-    if (departedJob == null)
-      throw new IllegalArgumentException ();
-    rescheduleDepartureEvent ();
-  }
-
-  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  //
-  // RESCHEDULE DEPARTURE EVENT
-  //
-  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  
-  /** Reschedules the single departure event for this queue.
-   * 
-   * <p>
-   * First, cancels a pending departure event.
-   * Then, determines which job in the service area (if any) is to depart first through {@link #virtualDepartureTime},
-   * and schedules a departure event for it (or makes the job depart immediately through {@link #depart} if its
-   * remaining service time is zero.
-   * 
-   * @see #getDepartureEvents
-   * @see #cancelDepartureEvent
-   * @see #jobsInServiceArea
-   * @see #virtualDepartureTime
-   * @see #getVirtualTime
-   * @see #depart
-   * @see #scheduleDepartureEvent
-   * 
-   */
-  protected final void rescheduleDepartureEvent ()
-  {
-    final Set<DefaultDepartureEvent<J, Q>> departureEvents = getDepartureEvents ();
-    if (departureEvents == null)
-      throw new RuntimeException ();
-    if (departureEvents.size () > 1)
-      throw new IllegalStateException ();
-    if (departureEvents.size () > 0)
-      cancelDepartureEvent (departureEvents.iterator ().next ());
-    if (! this.virtualDepartureTime.isEmpty ())
-    {
-      if (getNumberOfJobsInServiceArea () == 0)
-        throw new IllegalStateException ();
-      final double scheduleVirtualTime = this.virtualDepartureTime.firstValue ();
-      final double deltaVirtualTime = scheduleVirtualTime - getVirtualTime ();
-      if (deltaVirtualTime < 0)
-        throw new IllegalStateException ();
-      final J job = this.virtualDepartureTime.getPreImageForValue (scheduleVirtualTime).iterator ().next ();
-      if (deltaVirtualTime == 0)
-        depart (getLastUpdateTime (), job);
-      else
-      {
-        final double deltaTime = deltaVirtualTime * getNumberOfJobsInServiceArea ();
-        scheduleDepartureEvent (getLastUpdateTime () + deltaTime, job);
-      }
-    }
-  }
-  
 }
