b4b1c51fa66bc1d3ece15e9af2f981aacd5fed75
Jan de Jongh <jfcmdejongh@gmail.com>
2017-05-26 15:24:11 +0000

    [MAJOR CODE AND DOC REVIEW AND RESTRUCTURE CYCLE MAY 2017]
    Moved from deeper in tree.
    Javadoc/code/comment review.
    
    [PARTIAL COMMIT]
    
    git-svn-id: https://jandejongh.dnsdojo.org:3690/jqueues/trunk@871 23c8eed1-2f9f-4d5d-b507-dd3313c27e4c

---
+++ java/src/nl/jdj/jqueues/r5/entity/SimEntityEvent.java
@@ -0,0 +1,164 @@
+package nl.jdj.jqueues.r5.entity;
+
+import nl.jdj.jsimulation.r5.DefaultSimEvent;
+import nl.jdj.jsimulation.r5.SimEvent;
+import nl.jdj.jsimulation.r5.SimEventAction;
+
+/** A {@link SimEvent} for a {@link SimEntity} (queue, job, or other) operation.
+ * 
+ * <p>
+ * This class only administers the key parameters for the event; it does not actually schedule it.
+ *
+ * @author Jan de Jongh, TNO
+ * 
+ * <p>
+ * Copyright (C) 2005-2017 Jan de Jongh, TNO
+ * 
+ * <p>
+ * This file is covered by the LICENSE file in the root of this project.
+ * 
+ */
+public abstract class SimEntityEvent
+extends DefaultSimEvent
+{
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // CONSTRUCTOR(S) / FACTORY / CLONING
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  /** Creates a new event for an entity.
+   * 
+   * @param name   The (optional) name of the event, may be  {@code null}.
+   * @param time   The time at which the event occurs.
+   * @param entity The entity related to the event, non-{@code null}.
+   * @param action The {@link SimEventAction} to take; may be {@code null}.
+   * 
+   * @throws IllegalArgumentException If {@code entity == null}.
+   */
+  protected SimEntityEvent
+  (final String name,
+   final double time,
+   final SimEntity entity,
+   final SimEventAction<? extends SimEntity> action)
+  {
+    super (name, time, null, action);
+    if (entity == null)
+      throw new IllegalArgumentException ();
+    this.entity = entity;
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // ENTITY
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  private final SimEntity entity;
+  
+  /** Gets the entity at which the event occurs.
+   * 
+   * @return The entity to which the event applies, may be {@code null}.
+   * 
+   */
+  public final SimEntity getEntity ()
+  {
+    return this.entity;
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // RESET [EVENT]
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  /** The (default) {@link SimEvent} for resetting a {@link SimEntity}.
+   * 
+   */
+  public final static class Reset
+  extends SimEntityEvent
+  {
+ 
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //
+    // CONSTRUCTOR(S) / FACTORY / CLONING
+    //
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+    private static SimEventAction createAction (final SimEntity entity)
+    {
+      if (entity == null)
+        throw new IllegalArgumentException ();
+      return (final SimEvent event) -> entity.resetEntity ();
+    }
+  
+    /** Creates a reset event at a specific entity.
+     *   
+     * @param entity The entity, non-{@code null}.
+     * @param time   The time at which to reset the entity.
+     * 
+     * @throws IllegalArgumentException If <code>entity == null</code>.
+     * 
+     * @see SimEntity#resetEntity
+     * 
+     */
+    public Reset
+    (final SimEntity entity, final double time)
+    {
+      super ("Reset@" + entity, time, entity, createAction (entity));
+    }
+  
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // UPDATE [EVENT]
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  /** The (default) {@link SimEvent} for updating a {@link SimEntity}.
+   * 
+   */
+  public final static class Update
+  extends SimEntityEvent
+  {
+ 
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //
+    // CONSTRUCTOR(S) / FACTORY / CLONING
+    //
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+    private static SimEventAction createAction (final SimEntity entity, final double time)
+    {
+      if (entity == null)
+        throw new IllegalArgumentException ();
+      return (final SimEvent event) -> entity.update (time);
+    }
+  
+    /** Creates an update event at a specific entity.
+     *   
+     * @param entity The entity, non-{@code null}.
+     * @param time   The time at which to update the entity.
+     * 
+     * @throws IllegalArgumentException If <code>entity == null</code>.
+     * 
+     * @see SimEntity#update
+     * 
+     */
+    public Update
+    (final SimEntity entity, final double time)
+    {
+      super ("Update@" + entity, time, entity, createAction (entity, time));
+    }
+  
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // END OF FILE
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+}
