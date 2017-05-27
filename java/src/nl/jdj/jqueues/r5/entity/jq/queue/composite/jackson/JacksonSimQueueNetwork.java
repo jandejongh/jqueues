bb4f6e8e5160fb89bd2622525495f98cd7ccb9fe
Jan de Jongh <jfcmdejongh@gmail.com>
2017-05-27 14:57:34 +0000

    [MAJOR CODE AND DOC REVIEW AND RESTRUCTURE CYCLE MAY 2017]
    new version of jq.queue.composite.jackson
    
    [PARTIAL COMMIT]
    
    git-svn-id: https://jandejongh.dnsdojo.org:3690/jqueues/trunk@891 23c8eed1-2f9f-4d5d-b507-dd3313c27e4c

---
+++ java/src/nl/jdj/jqueues/r5/entity/jq/queue/composite/jackson/JacksonSimQueueNetwork.java
@@ -0,0 +1,159 @@
+package nl.jdj.jqueues.r5.entity.jq.queue.composite.jackson;
+
+import java.util.Random;
+import java.util.Set;
+import nl.jdj.jqueues.r5.entity.jq.job.SimJob;
+import nl.jdj.jqueues.r5.entity.jq.queue.SimQueue;
+import nl.jdj.jqueues.r5.entity.jq.job.AbstractSimJob;
+import nl.jdj.jqueues.r5.entity.jq.queue.composite.AbstractSimQueueComposite;
+import nl.jdj.jqueues.r5.entity.jq.queue.composite.DefaultDelegateSimJobFactory;
+import nl.jdj.jqueues.r5.entity.jq.queue.composite.DelegateSimJobFactory;
+import nl.jdj.jqueues.r5.entity.jq.queue.composite.SimQueueComposite;
+import nl.jdj.jsimulation.r5.SimEventList;
+
+/** A {@link SimQueueComposite} implementation of a Jackson queueing network.
+ *
+ * <p>
+ * For more documentation see {@link JacksonSimQueueSelector}.
+ * 
+ * @param <DJ> The delegate-job type.
+ * @param <DQ> The queue-type for delegate jobs.
+ * @param <J>  The job type.
+ * @param <Q>  The queue type for jobs.
+ * 
+ * @author Jan de Jongh, TNO
+ * 
+ * <p>
+ * Copyright (C) 2005-2017 Jan de Jongh, TNO
+ * 
+ * <p>
+ * This file is covered by the LICENSE file in the root of this project.
+ * 
+ */
+public class JacksonSimQueueNetwork
+  <DJ extends AbstractSimJob, DQ extends SimQueue, J extends SimJob, Q extends JacksonSimQueueNetwork>
+  extends AbstractSimQueueComposite<DJ, DQ, J, Q>
+{
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // CONSTRUCTOR(S) / FACTORY
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  /** Creates a black Jackson queueing network.
+   * 
+   * For brevity, <code>|Q|</code> is used as a shorthand for <code>queues.size ()</code>.
+   * 
+   * @param eventList             The event list to use.
+   * @param queues                The queues, an iteration over the set must return (deterministically)
+   *                                the non-<code>null</code> queues
+   *                                as indexed in the probability arguments;
+   *                                see also {@link JacksonSimQueueSelector#JacksonSimQueueSelector}.
+   * @param pdfArrival            See {@link JacksonSimQueueSelector#JacksonSimQueueSelector}.
+   * @param pdfTransition         See {@link JacksonSimQueueSelector#JacksonSimQueueSelector}.
+   * @param userRNG               See {@link JacksonSimQueueSelector#JacksonSimQueueSelector}.
+   * @param delegateSimJobFactory An optional factory for the delegate {@link SimJob}s.
+   *
+   * @throws IllegalArgumentException If the event list is <code>null</code>,
+   *                                  the <code>queues</code> argument is <code>null</code>,
+   *                                  or if it contains a <code>null</code> entry,
+   *                                  or if one or both of the probability arguments is improperly dimensioned or contains
+   *                                  illegal values.
+   * 
+   * @see DelegateSimJobFactory
+   * @see DefaultDelegateSimJobFactory
+   * 
+   */
+  public JacksonSimQueueNetwork
+  (final SimEventList eventList,
+   final Set<DQ> queues,
+   final double[] pdfArrival,
+   final double[][] pdfTransition,
+   final Random userRNG,
+   final DelegateSimJobFactory delegateSimJobFactory)
+  {
+    super (eventList,
+      queues,
+      new JacksonSimQueueSelector<>  (queues, pdfArrival, pdfTransition, userRNG),
+      delegateSimJobFactory);
+  }
+
+  /** Returns a new {@link JacksonSimQueueNetwork} object on the same {@link SimEventList} with copies of the sub-queues and
+   *  probability arguments, a new RNG, and the same delegate-job factory.
+   * 
+   * @return A new {@link JacksonSimQueueNetwork} object on the same {@link SimEventList} with copies of the sub-queues and
+   *  probability arguments, a new RNG, and the same delegate-job factory.
+   * 
+   * @throws UnsupportedOperationException If the encapsulated queues could not be copied through {@link SimQueue#getCopySimQueue}.
+   * 
+   * @see #getEventList
+   * @see #getCopySubSimQueues
+   * @see JacksonSimQueueSelector#getPdfArrival
+   * @see JacksonSimQueueSelector#getPdfTransition
+   * @see #getDelegateSimJobFactory
+   * 
+   */
+  @Override
+  public JacksonSimQueueNetwork<DJ, DQ, J, Q> getCopySimQueue ()
+  {
+    final Set<DQ> queuesCopy = getCopySubSimQueues ();
+    return new JacksonSimQueueNetwork<>
+      (getEventList (),
+        queuesCopy,
+        ((JacksonSimQueueSelector) getSimQueueSelector ()).getPdfArrival (),
+        ((JacksonSimQueueSelector) getSimQueueSelector ()).getPdfTransition (),
+        null,
+        getDelegateSimJobFactory ());
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // NAME
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  /** Returns "Jackson[queue list]".
+   * 
+   * @return "Jackson[queue list]".
+   * 
+   */
+  @Override
+  public String toStringDefault ()
+  {
+    String string = "Jackson[";
+    boolean first = true;
+    for (DQ dq : getQueues ())
+    {
+      if (! first)
+        string += ",";
+      else
+        first = false;
+      string += dq;
+    }
+    string += "]";
+    return string;
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // RESET
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  /** Calls super method and clear the administration of visits.
+   * 
+   */
+  @Override
+  protected final void resetEntitySubClass ()
+  {
+    super.resetEntitySubClass ();
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // END OF FILE
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+}
