e8c61ce797830453ee2626402c8a6c9650b06eaa
Jan de Jongh <jfcmdejongh@gmail.com>
2017-05-27 21:25:25 +0000

    [MAJOR CODE AND DOC REVIEW AND RESTRUCTURE CYCLE MAY 2017]
    new version of entity.jq.queue.composite [test]
    
    [PARTIAL COMMIT]
    
    git-svn-id: https://jandejongh.dnsdojo.org:3690/jqueues/trunk@923 23c8eed1-2f9f-4d5d-b507-dd3313c27e4c

---
+++ java/test/nl/jdj/jqueues/r5/entity/jq/queue/composite/ComprTandem2Test.java
@@ -0,0 +1,229 @@
+package nl.jdj.jqueues.r5.entity.jq.queue.composite;
+
+import java.util.Collections;
+import java.util.Set;
+import nl.jdj.jqueues.r5.entity.jq.queue.SimQueue;
+import nl.jdj.jqueues.r5.entity.jq.queue.DefaultSimQueueTests;
+import nl.jdj.jqueues.r5.entity.jq.queue.composite.dual.ctandem2.CompressedTandem2SimQueue;
+import nl.jdj.jqueues.r5.entity.jq.queue.nonpreemptive.FCFS;
+import nl.jdj.jqueues.r5.entity.jq.queue.nonpreemptive.FCFS_B;
+import nl.jdj.jqueues.r5.entity.jq.queue.nonpreemptive.FCFS_B_c;
+import nl.jdj.jqueues.r5.entity.jq.queue.nonpreemptive.FCFS_c;
+import nl.jdj.jqueues.r5.entity.jq.queue.nonpreemptive.LCFS;
+import nl.jdj.jqueues.r5.entity.jq.queue.nonpreemptive.NoBuffer_c;
+import nl.jdj.jqueues.r5.entity.jq.queue.serverless.DROP;
+import nl.jdj.jqueues.r5.entity.jq.queue.serverless.DLIMIT;
+import nl.jdj.jqueues.r5.entity.jq.queue.serverless.SINK;
+import nl.jdj.jqueues.r5.entity.jq.queue.serverless.ZERO;
+import nl.jdj.jqueues.r5.util.loadfactory.LoadFactoryHint;
+import nl.jdj.jqueues.r5.util.loadfactory.pattern.KnownLoadFactory_SQ_SV;
+import nl.jdj.jqueues.r5.util.loadfactory.pattern.LoadFactory_SQ_SV_0010;
+import nl.jdj.jqueues.r5.util.predictor.AbstractSimQueuePredictor;
+import nl.jdj.jqueues.r5.util.predictor.SimQueuePredictionException;
+import nl.jdj.jqueues.r5.util.predictor.queues.SimQueuePredictor_ComprTandem2;
+import nl.jdj.jqueues.r5.util.predictor.queues.SimQueuePredictor_DROP;
+import nl.jdj.jqueues.r5.util.predictor.queues.SimQueuePredictor_FCFS;
+import nl.jdj.jqueues.r5.util.predictor.queues.SimQueuePredictor_LCFS;
+import nl.jdj.jqueues.r5.util.predictor.queues.SimQueuePredictor_DLIMIT;
+import nl.jdj.jqueues.r5.util.predictor.queues.SimQueuePredictor_SINK;
+import nl.jdj.jqueues.r5.util.predictor.queues.SimQueuePredictor_ZERO;
+import nl.jdj.jsimulation.r5.DefaultSimEvent;
+import nl.jdj.jsimulation.r5.DefaultSimEventList;
+import nl.jdj.jsimulation.r5.SimEventList;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/** Tests for {@link CompressedTandem2SimQueue}.
+ *
+ * @author Jan de Jongh, TNO
+ * 
+ * <p>
+ * Copyright (C) 2005-2017 Jan de Jongh, TNO
+ * 
+ * <p>
+ * This file is covered by the LICENSE file in the root of this project.
+ * 
+ */
+public class ComprTandem2Test
+{
+  
+  public ComprTandem2Test ()
+  {
+  }
+  
+  @BeforeClass
+  public static void setUpClass ()
+  {
+  }
+  
+  @AfterClass
+  public static void tearDownClass ()
+  {
+  }
+  
+  @Before
+  public void setUp ()
+  {
+  }
+  
+  @After
+  public void tearDown ()
+  {
+  }
+  
+  public void testComprTandem2Aux
+  (final SimQueue waitQueue,
+   final SimQueue serveQueue,
+   final AbstractSimQueuePredictor waitQueuePredictor,
+   final AbstractSimQueuePredictor serveQueuePredictor,
+   final int numberOfJobs,
+   final Set<LoadFactoryHint> hints,
+   final boolean silent,
+   final boolean deadSilent,
+   final double accuracy,
+   final Set<KnownLoadFactory_SQ_SV> omit,
+   final Set<KnownLoadFactory_SQ_SV> restrict,
+   final String message)    
+   throws SimQueuePredictionException
+  {
+    final CompressedTandem2SimQueue ctandem2 =
+      new CompressedTandem2SimQueue (waitQueue.getEventList (), waitQueue, serveQueue, null);
+    final SimQueuePredictor_ComprTandem2 predictor_ctandem2 =
+      new SimQueuePredictor_ComprTandem2 (waitQueuePredictor, serveQueuePredictor);
+    DefaultSimQueueTests.doSimQueueTests_SQ_SV
+      (ctandem2, predictor_ctandem2, null, numberOfJobs, hints, silent, deadSilent, accuracy, omit, restrict, message);
+  }
+  
+  public void testComprTandem2Aux
+  (final SimQueue waitQueue,
+   final SimQueue serveQueue,
+   final SimQueue predictorQueue,
+   final int numberOfJobs,
+   final Set<LoadFactoryHint> hints,
+   final boolean silent,
+   final boolean deadSilent,
+   final double accuracy,
+   final Set<KnownLoadFactory_SQ_SV> omit,
+   final Set<KnownLoadFactory_SQ_SV> restrict,
+   final String message)    
+   throws SimQueuePredictionException
+  {
+    final CompressedTandem2SimQueue ctandem2 =
+      new CompressedTandem2SimQueue (waitQueue.getEventList (), waitQueue, serveQueue, null);
+    DefaultSimQueueTests.doSimQueueTests_SQ_SV
+      (ctandem2, null, predictorQueue, numberOfJobs, hints, silent, deadSilent, accuracy, omit, restrict, message);
+  }
+  
+  /**
+   * Test of CompressedTandem2SimQueue.
+   * 
+   */
+  @Test
+  public void testComprTandem2 () throws SimQueuePredictionException
+  {
+    final SimEventList eventList = new DefaultSimEventList (DefaultSimEvent.class);
+    final int numberOfJobs = 50;
+    final Set<LoadFactoryHint> jitterHint = Collections.singleton (LoadFactory_SQ_SV_0010.SERVICE_TIME_JITTER);
+    final boolean silent = true;
+    final boolean deadSilent = true;
+    //
+    // ComprTandem2[FCFS, FCFS]
+    //
+    testComprTandem2Aux
+    ( new FCFS (eventList), new FCFS (eventList),
+      new SimQueuePredictor_FCFS (), new SimQueuePredictor_FCFS (),
+      numberOfJobs, jitterHint, silent, deadSilent, 1.0e-12, null, null, null);
+    //
+    // ComprTandem2[LCFS, FCFS]
+    //
+    testComprTandem2Aux
+    ( new LCFS (eventList), new FCFS (eventList),
+      new SimQueuePredictor_LCFS (), new SimQueuePredictor_FCFS (),
+      numberOfJobs, jitterHint, silent, deadSilent, 1.0e-12, null, null, null);
+    //
+    // ComprTandem2[DROP, FCFS]
+    //
+    testComprTandem2Aux
+    ( new DROP (eventList), new FCFS (eventList),
+      new SimQueuePredictor_DROP (), new SimQueuePredictor_FCFS (),
+      numberOfJobs, jitterHint, silent, deadSilent, 1.0e-12, null, null, null);
+    //
+    // ComprTandem2[FCFS, DROP]
+    //
+    testComprTandem2Aux
+    ( new FCFS (eventList), new DROP (eventList),
+      new SimQueuePredictor_FCFS (), new SimQueuePredictor_DROP (),
+      numberOfJobs, jitterHint, silent, deadSilent, 1.0e-12, null, null, null);
+    //
+    // ComprTandem2[SINK, FCFS]
+    //
+    testComprTandem2Aux
+    ( new SINK (eventList), new FCFS (eventList),
+      new SimQueuePredictor_SINK (), new SimQueuePredictor_FCFS (),
+      numberOfJobs, jitterHint, silent, deadSilent, 1.0e-12, null, null, null);
+    //
+    // ComprTandem2[FCFS, SINK]
+    //
+    testComprTandem2Aux
+    ( new FCFS (eventList),new SINK (eventList),
+      new SimQueuePredictor_FCFS (), new SimQueuePredictor_SINK (),
+      numberOfJobs, jitterHint, silent, deadSilent, 1.0e-12, null, null, null);
+    //
+    // ComprTandem2[ZERO, FCFS]
+    //
+    testComprTandem2Aux
+    ( new ZERO (eventList), new FCFS (eventList),
+      new SimQueuePredictor_ZERO (), new SimQueuePredictor_FCFS (),
+      numberOfJobs, jitterHint, silent, deadSilent, 1.0e-12, null, null, null);
+    //
+    // ComprTandem2[FCFS, ZERO]
+    //
+    testComprTandem2Aux
+    ( new FCFS (eventList), new ZERO (eventList),
+      new SimQueuePredictor_FCFS (), new SimQueuePredictor_ZERO (),
+      numberOfJobs, jitterHint, silent, deadSilent, 1.0e-12, null, null, null);
+    //
+    // ComprTandem2[ZERO, ZERO]
+    //
+    testComprTandem2Aux
+    ( new ZERO (eventList), new ZERO (eventList),
+      new SimQueuePredictor_ZERO (), new SimQueuePredictor_ZERO (),
+      numberOfJobs, jitterHint, silent, deadSilent, 1.0e-12, null, null, null);
+    //
+    // ComprTandem2[DLIMIT[0.5], ZERO]
+    //
+    testComprTandem2Aux
+    ( new DLIMIT (eventList, 0.5), new ZERO (eventList),
+      new SimQueuePredictor_DLIMIT (), new SimQueuePredictor_ZERO (),
+      numberOfJobs, jitterHint, silent, deadSilent, 1.0e-12, null, null, null);
+    //
+    // ComprTandem2[DLIMIT[0.5], DLIMIT[0.1]]
+    //
+    testComprTandem2Aux
+    ( new DLIMIT (eventList, 0.5), new DLIMIT (eventList, 0.1),
+      new SimQueuePredictor_DLIMIT (), new SimQueuePredictor_DLIMIT (),
+      numberOfJobs, jitterHint, silent, deadSilent, 1.0e-12, null, null, null);
+    //
+    // ComprTandem2[FCFS_B[0],FCFS_2] == NoBuffer_2
+    //
+    testComprTandem2Aux
+    ( new FCFS_B (eventList, 0), new FCFS_c (eventList, 2),
+      new NoBuffer_c (eventList, 2),
+      numberOfJobs, jitterHint, silent, deadSilent, 1.0e-12, null, null, null);
+    //
+    // ComprTandem2[FCFS_B, FCFS_c] == FCFS_B_c
+    //
+    final int[] bValues = { 0, 1, 2, 100 };
+    final int[] cValues = { 0, 1, 2, 10, 100 };
+    for (final int B : bValues)
+      for (final int c : cValues)
+        testComprTandem2Aux
+        ( new FCFS_B (eventList, B), new FCFS_c (eventList, c),
+          new FCFS_B_c (eventList, B, c),
+          numberOfJobs, jitterHint, silent, deadSilent, 1.0e-12, null, null, null);
+  }
+
+}
---
+++ java/test/nl/jdj/jqueues/r5/entity/jq/queue/composite/FB_vTest.java
@@ -0,0 +1,161 @@
+package nl.jdj.jqueues.r5.entity.jq.queue.composite;
+
+import java.util.Collections;
+import java.util.Set;
+import nl.jdj.jqueues.r5.entity.jq.queue.SimQueue;
+import nl.jdj.jqueues.r5.entity.jq.queue.DefaultSimQueueTests;
+import nl.jdj.jqueues.r5.entity.jq.queue.composite.single.feedback.NumVisitsFeedbackSimQueue;
+import nl.jdj.jqueues.r5.entity.jq.queue.nonpreemptive.FCFS;
+import nl.jdj.jqueues.r5.entity.jq.queue.serverless.DELAY;
+import nl.jdj.jqueues.r5.entity.jq.queue.serverless.DROP;
+import nl.jdj.jqueues.r5.entity.jq.queue.serverless.DLIMIT;
+import nl.jdj.jqueues.r5.entity.jq.queue.serverless.SINK;
+import nl.jdj.jqueues.r5.entity.jq.queue.serverless.ZERO;
+import nl.jdj.jqueues.r5.util.loadfactory.LoadFactoryHint;
+import nl.jdj.jqueues.r5.util.loadfactory.pattern.KnownLoadFactory_SQ_SV;
+import nl.jdj.jqueues.r5.util.loadfactory.pattern.LoadFactory_SQ_SV_0010;
+import nl.jdj.jqueues.r5.util.predictor.AbstractSimQueuePredictor;
+import nl.jdj.jqueues.r5.util.predictor.SimQueuePredictionException;
+import nl.jdj.jqueues.r5.util.predictor.queues.SimQueuePredictor_DELAY;
+import nl.jdj.jqueues.r5.util.predictor.queues.SimQueuePredictor_DROP;
+import nl.jdj.jqueues.r5.util.predictor.queues.SimQueuePredictor_FB_v;
+import nl.jdj.jqueues.r5.util.predictor.queues.SimQueuePredictor_FCFS;
+import nl.jdj.jqueues.r5.util.predictor.queues.SimQueuePredictor_DLIMIT;
+import nl.jdj.jqueues.r5.util.predictor.queues.SimQueuePredictor_SINK;
+import nl.jdj.jqueues.r5.util.predictor.queues.SimQueuePredictor_ZERO;
+import nl.jdj.jsimulation.r5.DefaultSimEvent;
+import nl.jdj.jsimulation.r5.DefaultSimEventList;
+import nl.jdj.jsimulation.r5.SimEventList;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/** Tests for {@link NumVisitsFeedbackSimQueue}.
+ *
+ * @author Jan de Jongh, TNO
+ * 
+ * <p>
+ * Copyright (C) 2005-2017 Jan de Jongh, TNO
+ * 
+ * <p>
+ * This file is covered by the LICENSE file in the root of this project.
+ *
+ */
+public class FB_vTest
+{
+  
+  public FB_vTest ()
+  {
+  }
+  
+  @BeforeClass
+  public static void setUpClass ()
+  {
+  }
+  
+  @AfterClass
+  public static void tearDownClass ()
+  {
+  }
+  
+  @Before
+  public void setUp ()
+  {
+  }
+  
+  @After
+  public void tearDown ()
+  {
+  }
+  
+  public void testFB_vAux
+  (final SimQueue encQueue,
+   final int numberOfVisits,
+   final AbstractSimQueuePredictor encQueuePredictor,
+   final int numberOfJobs,
+   final Set<LoadFactoryHint> hints,
+   final boolean silent,
+   final boolean deadSilent,
+   final double accuracy,
+   final Set<KnownLoadFactory_SQ_SV> omit,
+   final Set<KnownLoadFactory_SQ_SV> restrict,
+   final String message)    
+   throws SimQueuePredictionException
+  {
+    final NumVisitsFeedbackSimQueue fb_v =
+      new NumVisitsFeedbackSimQueue (encQueue.getEventList (), encQueue, numberOfVisits, null);
+    final SimQueuePredictor_FB_v predictor_fb_v =
+      new SimQueuePredictor_FB_v (encQueuePredictor);
+    DefaultSimQueueTests.doSimQueueTests_SQ_SV
+      (fb_v, predictor_fb_v, null, numberOfJobs, hints, silent, deadSilent, accuracy, omit, restrict, message);
+  }
+  
+  /**
+   * Test of NumVisitsFeedbackSimQueue.
+   * 
+   */
+  @Test
+  public void testFB_vAux () throws SimQueuePredictionException
+  {
+    final SimEventList eventList = new DefaultSimEventList (DefaultSimEvent.class);
+    final int numberOfJobs = 10;
+    final Set<LoadFactoryHint> jitterHint = Collections.singleton (LoadFactory_SQ_SV_0010.SERVICE_TIME_JITTER);
+    final boolean silent = true;
+    final boolean deadSilent = true;
+    final int[] numVisitsValues = {1, 10, 100};
+    for (final int numVisits : numVisitsValues)
+    {
+      //
+      // FB_numVisits[DROP]
+      //
+      testFB_vAux
+      ( new DROP (eventList),
+        numVisits,
+        new SimQueuePredictor_DROP (),
+        numberOfJobs, jitterHint, silent, deadSilent, 1.0e-12, null, null, null);
+      //
+      // FB_numVisits[ZERO]
+      //
+      testFB_vAux
+      ( new ZERO (eventList),
+        numVisits,
+        new SimQueuePredictor_ZERO (),
+        numberOfJobs, jitterHint, silent, deadSilent, 1.0e-12, null, null, null);
+      //
+      // FB_numVisits[SINK]
+      //
+      testFB_vAux
+      ( new SINK (eventList),
+        numVisits,
+        new SimQueuePredictor_SINK (),
+        numberOfJobs, jitterHint, silent, deadSilent, 1.0e-12, null, null, null);
+      //
+      // FB_numVisits[FCFS]
+      //
+      testFB_vAux
+      ( new FCFS (eventList),
+        numVisits,
+        new SimQueuePredictor_FCFS (),
+        numberOfJobs, jitterHint, silent, deadSilent, 1.0e-12, null, null, null);
+      //
+      // FB_numVisits[DELAY[3.3]]
+      //
+      testFB_vAux
+      ( new DELAY (eventList, 3.3),
+        numVisits,
+        new SimQueuePredictor_DELAY (),
+        numberOfJobs, jitterHint, silent, deadSilent, 1.0e-12, null, null, null);
+      //
+      // FB_numVisits[DLIMIT[0.1]]
+      //
+      testFB_vAux
+      ( new DLIMIT (eventList, 0.1),
+        numVisits,
+        new SimQueuePredictor_DLIMIT (),
+        numberOfJobs, jitterHint, silent, deadSilent, 1.0e-12, null, null, null);
+    }
+  }
+
+}
