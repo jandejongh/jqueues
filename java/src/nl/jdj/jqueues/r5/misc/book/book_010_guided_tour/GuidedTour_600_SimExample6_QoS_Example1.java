babef365b01c2d202f5b29bd8272db66cd82944a
Jan de Jongh <jfcmdejongh@gmail.com>
2017-06-08 11:35:17 +0000

    Progress on Guided Tour in Book. [CLEAN COMMIT]
    git-svn-id: https://jandejongh.dnsdojo.org:3690/jqueues/trunk@984 23c8eed1-2f9f-4d5d-b507-dd3313c27e4c

---
+++ java/src/nl/jdj/jqueues/r5/misc/book/book_010_guided_tour/GuidedTour_600_SimExample6_QoS_Example1.java
@@ -0,0 +1,31 @@
+package nl.jdj.jqueues.r5.misc.book.book_010_guided_tour;
+
+import nl.jdj.jqueues.r5.entity.jq.job.SimJob;
+import nl.jdj.jqueues.r5.entity.jq.SimJQEventScheduler;
+import nl.jdj.jqueues.r5.entity.jq.job.qos.DefaultSimJobQoS;
+import nl.jdj.jqueues.r5.entity.jq.queue.preemptive.PreemptionStrategy;
+import nl.jdj.jqueues.r5.entity.jq.queue.qos.PQ;
+import nl.jdj.jsimulation.r5.DefaultSimEventList;
+import nl.jdj.jsimulation.r5.SimEventList;
+
+final class GuidedTour_600_SimExample6_QoS_Example1
+{
+  
+  public static void main (final String[] args)
+  {
+    final SimEventList el = new DefaultSimEventList ();
+    final PQ queue = new PQ (el, PreemptionStrategy.RESUME, Double.class, Double.NEGATIVE_INFINITY);
+    queue.registerStdOutSimEntityListener ();
+    el.reset (0.0);
+    for (int j = 1; j <= 4; j++)
+    {
+      final double jobServiceTime = 12.0 / j;
+      final double jobArrivalTime = (double) j;
+      final String jobName = Integer.toString (j);
+      final SimJob job = new DefaultSimJobQoS (null, jobName, jobServiceTime, Double.class, (double) (-j));
+      SimJQEventScheduler.scheduleJobArrival (job, queue, jobArrivalTime);
+    }
+    el.run ();
+  }
+  
+}
