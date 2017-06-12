741d30a76c2d8deef448e2059d733aad95100c7f
Jan de Jongh <jfcmdejongh@gmail.com>
2017-06-12 15:40:14 +0000

    MAJOR REORGANIZATION AND RE-IMPLEMENTATION OF COMPOSITE QUEUES. [PARTIAL COMMIT]
    git-svn-id: https://jandejongh.dnsdojo.org:3690/jqueues/trunk@1021 23c8eed1-2f9f-4d5d-b507-dd3313c27e4c

--- java/src/nl/jdj/jqueues/r5/entity/jq/queue/composite/enc/Enc.java
+++ java/src/nl/jdj/jqueues/r5/entity/jq/queue/composite/enc/Enc.java
@@ -1,21 +1,20 @@
 package nl.jdj.jqueues.r5.entity.jq.queue.composite.enc;
 
+import java.util.List;
 import nl.jdj.jqueues.r5.entity.jq.job.SimJob;
 import nl.jdj.jqueues.r5.entity.jq.queue.SimQueue;
 import nl.jdj.jqueues.r5.entity.jq.queue.composite.AbstractSimQueueComposite;
 import nl.jdj.jqueues.r5.entity.jq.queue.composite.DefaultDelegateSimJobFactory;
 import nl.jdj.jqueues.r5.entity.jq.queue.composite.DelegateSimJobFactory;
 import nl.jdj.jqueues.r5.entity.jq.queue.composite.SimQueueComposite;
-import nl.jdj.jqueues.r5.entity.jq.queue.composite.SimQueueComposite.StartModel;
+import nl.jdj.jqueues.r5.listener.MultiSimQueueNotificationProcessor;
 import nl.jdj.jsimulation.r5.SimEventList;
 
 /** A {@link SimQueueComposite} encapsulating a single {@link SimQueue}.
  *
  * <p>
- * This composite queue (precisely) mimics the {@link SimQueue} interface of the encapsulated queue.
- * 
- * <p>
- * The start model is set to (fixed) {@link StartModel#ENCAPSULATOR_QUEUE}.
+ * This composite queue (precisely) mimics the {@link SimQueue} interface of the encapsulated queue,
+ * including non-standard operations and notifications.
  * 
  * <p>
  * Refer to {@link AbstractEncapsulatorSimQueue},
@@ -29,8 +28,6 @@ import nl.jdj.jsimulation.r5.SimEventList;
  * @param <Q>  The queue type for jobs.
  * 
  * @see SimQueueComposite
- * @see StartModel
- * @see StartModel#ENCAPSULATOR_QUEUE
  * @see EncHS
  * 
  * @author Jan de Jongh, TNO
@@ -55,9 +52,6 @@ public class Enc
   
   /** Creates an encapsulator queue given an event list and a queue.
    *
-   * <p>
-   * The constructor sets the {@link StartModel} to {@link StartModel#ENCAPSULATOR_QUEUE}.
-   * 
    * @param eventList             The event list to use.
    * @param queue                 The encapsulated queue.
    * @param delegateSimJobFactory An optional factory for the delegate {@link SimJob}s.
@@ -66,8 +60,6 @@ public class Enc
    * 
    * @see DelegateSimJobFactory
    * @see DefaultDelegateSimJobFactory
-   * @see StartModel
-   * @see StartModel#ENCAPSULATOR_QUEUE
    * 
    */
   public Enc
@@ -75,7 +67,7 @@ public class Enc
    final DQ queue,
    final DelegateSimJobFactory delegateSimJobFactory)
   {
-    super (eventList, queue, delegateSimJobFactory, false);
+    super (eventList, queue, delegateSimJobFactory);
   }
   
   /** Returns a new {@link Enc} object on the same {@link SimEventList} with a copy of the encapsulated
@@ -119,6 +111,34 @@ public class Enc
 
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   //
+  // QoS / QoS CLASS
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  /** Calls super method (in order to make implementation final).
+   * 
+   * @return The result from the super method.
+   * 
+   */
+  @Override
+  public final Object getQoS ()
+  {
+    return super.getQoS ();
+  }
+
+  /** Calls super method (in order to make implementation final).
+   * 
+   * @return The result from the super method.
+   * 
+   */
+  @Override
+  public final Class getQoSClass ()
+  {
+    return super.getQoSClass ();
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
   // RESET
   //
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -131,7 +151,159 @@ public class Enc
   {
     super.resetEntitySubClass ();
   }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // QUEUE-ACCESS VACATION
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   
+  /** Calls super method (in order to make implementation final).
+   * 
+   */
+  @Override
+  protected final void queueAccessVacationDropSubClass (double time, J job)
+  {
+    super.queueAccessVacationDropSubClass (time, job);
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // ARRIVAL
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  /** Calls super method (in order to make implementation final).
+   * 
+   */
+  @Override
+  protected final void insertJobInQueueUponArrival (final J job, final double time)
+  {
+    super.insertJobInQueueUponArrival (job, time);
+  }
+
+  /** Calls super method (in order to make implementation final).
+   * 
+   */
+  @Override
+  protected final void rescheduleAfterArrival (final J job, final double time)
+  {
+    super.rescheduleAfterArrival (job, time);
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // DROP
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  /** Calls super method (in order to make implementation final).
+   * 
+   */
+  @Override
+  protected final void removeJobFromQueueUponDrop (final J job, final double time)
+  {
+    super.removeJobFromQueueUponDrop (job, time);
+  }
+
+  /** Calls super method (in order to make implementation final).
+   * 
+   */
+  @Override
+  protected final void rescheduleAfterDrop (final J job, final double time)
+  {
+    super.rescheduleAfterDrop (job, time);
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // REVOCATION
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  /** Calls super method (in order to make implementation final).
+   * 
+   */
+  @Override
+  protected final void removeJobFromQueueUponRevokation (final J job, final double time, final boolean auto)
+  {
+    super.removeJobFromQueueUponRevokation (job, time, auto);
+  }
+
+  /** Calls super method (in order to make implementation final).
+   * 
+   */
+  @Override
+  protected final void rescheduleAfterRevokation (final J job, final double time, final boolean auto)
+  {
+    super.rescheduleAfterRevokation (job, time, auto);
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // StartArmed
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  /** Calls super method (in order to make implementation final).
+   * 
+   * @return The result from the super method.
+   * 
+   */
+  @Override
+  public final boolean isStartArmed ()
+  {
+    return super.isStartArmed ();
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // SERVER-ACCESS CREDITS
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  /** Calls super method (in order to make implementation final).
+   * 
+   */
+  @Override
+  protected final void setServerAccessCreditsSubClass ()
+  {
+    super.setServerAccessCreditsSubClass ();
+  }
+  
+  /** Calls super method (in order to make implementation final).
+   * 
+   */
+  @Override
+  protected final void rescheduleForNewServerAccessCredits (final double time)
+  {
+    super.rescheduleForNewServerAccessCredits (time);
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // START
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  /** Calls super method (in order to make implementation final).
+   * 
+   */
+  @Override
+  protected final void insertJobInQueueUponStart (final J job, final double time)
+  {
+    super.insertJobInQueueUponStart (job, time);
+  }
+
+  /** Calls super method (in order to make implementation final).
+   * 
+   */
+  @Override
+  protected final void rescheduleAfterStart (final J job, final double time)
+  {
+    super.rescheduleAfterStart (job, time);
+  }
+
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   //
   // SERVICE TIME FOR JOB
@@ -147,6 +319,46 @@ public class Enc
   protected final double getServiceTimeForJob (final J job)
   {
     return super.getServiceTimeForJob (job);
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // DEPARTURE
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  /** Calls super method (in order to make implementation final).
+   * 
+   */
+  @Override
+  protected final void removeJobFromQueueUponDeparture (final J departingJob, final double time)
+  {
+    super.removeJobFromQueueUponDeparture (departingJob, time);
+  }
+
+  /** Calls super method (in order to make implementation final).
+   * 
+   */
+  @Override
+  protected final void rescheduleAfterDeparture (final J departedJob, final double time)
+  {
+    super.rescheduleAfterDeparture (departedJob, time);
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // SUB-QUEUE STATE-CHANGE NOTIFICATIONS
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  /** Calls super method (in order to make implementation final).
+   * 
+   */
+  @Override
+  protected final void processSubQueueNotifications
+  (final List<MultiSimQueueNotificationProcessor.Notification<DJ, DQ>> notifications)
+  {
+    super.processSubQueueNotifications (notifications);
   }
   
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
--- java/src/nl/jdj/jqueues/r5/entity/jq/queue/composite/enc/EncHS.java
+++ java/src/nl/jdj/jqueues/r5/entity/jq/queue/composite/enc/EncHS.java
@@ -1,22 +1,26 @@
 package nl.jdj.jqueues.r5.entity.jq.queue.composite.enc;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import nl.jdj.jqueues.r5.entity.SimEntitySimpleEventType;
+import nl.jdj.jqueues.r5.entity.jq.SimJQEvent;
+import nl.jdj.jqueues.r5.entity.jq.SimJQSimpleEventType;
 import nl.jdj.jqueues.r5.entity.jq.job.SimJob;
 import nl.jdj.jqueues.r5.entity.jq.queue.SimQueue;
 import nl.jdj.jqueues.r5.entity.jq.queue.composite.AbstractSimQueueComposite;
 import nl.jdj.jqueues.r5.entity.jq.queue.composite.DefaultDelegateSimJobFactory;
 import nl.jdj.jqueues.r5.entity.jq.queue.composite.DelegateSimJobFactory;
 import nl.jdj.jqueues.r5.entity.jq.queue.composite.SimQueueComposite;
-import nl.jdj.jqueues.r5.entity.jq.queue.composite.SimQueueComposite.StartModel;
+import nl.jdj.jqueues.r5.listener.MultiSimQueueNotificationProcessor;
 import nl.jdj.jsimulation.r5.SimEventList;
 
 /** A {@link SimQueueComposite} encapsulating a single {@link SimQueue} of which job starts are hidden.
  *
  * <p>
  * This composite queue (precisely) mimics the {@link SimQueue} interface of the encapsulated queue,
+ * including non-standard operations and notifications,
  * but hides the start of (delegate) job on the encapsulated queue.
- * 
- * <p>
- * The start model is set to (fixed) {@link StartModel#ENCAPSULATOR_HIDE_START_QUEUE}.
  * 
  * <p>
  * Refer to {@link AbstractEncapsulatorSimQueue},
@@ -30,8 +34,6 @@ import nl.jdj.jsimulation.r5.SimEventList;
  * @param <Q>  The queue type for jobs.
  * 
  * @see SimQueueComposite
- * @see StartModel
- * @see StartModel#ENCAPSULATOR_HIDE_START_QUEUE
  * @see Enc
  * 
  * @author Jan de Jongh, TNO
@@ -56,10 +58,6 @@ public class EncHS
   
   /** Creates an encapsulator queue that hides its sub-queue starts given an event list and a queue.
    *
-   * <p>
-   * The constructor sets the {@link StartModel}
-   * to {@link StartModel#ENCAPSULATOR_HIDE_START_QUEUE}.
-   * 
    * @param eventList             The event list to use.
    * @param queue                 The encapsulated queue.
    * @param delegateSimJobFactory An optional factory for the delegate {@link SimJob}s.
@@ -68,8 +66,6 @@ public class EncHS
    * 
    * @see DelegateSimJobFactory
    * @see DefaultDelegateSimJobFactory
-   * @see StartModel
-   * @see StartModel#ENCAPSULATOR_HIDE_START_QUEUE
    * 
    */
   public EncHS
@@ -77,7 +73,7 @@ public class EncHS
    final DQ queue,
    final DelegateSimJobFactory delegateSimJobFactory)
   {
-    super (eventList, queue, delegateSimJobFactory, true);
+    super (eventList, queue, delegateSimJobFactory);
   }
   
   /** Returns a new {@link EncHS} object on the same {@link SimEventList}
@@ -97,8 +93,7 @@ public class EncHS
   public EncHS<DJ, DQ, J, Q> getCopySimQueue ()
   {
     final SimQueue<DJ, DQ> encapsulatedQueueCopy = getEncapsulatedQueue ().getCopySimQueue ();
-    return new EncHS
-                 (getEventList (), encapsulatedQueueCopy, getDelegateSimJobFactory ());
+    return new EncHS (getEventList (), encapsulatedQueueCopy, getDelegateSimJobFactory ());
   }
   
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -120,6 +115,34 @@ public class EncHS
 
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   //
+  // QoS / QoS CLASS
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  /** Calls super method (in order to make implementation final).
+   * 
+   * @return The result from the super method.
+   * 
+   */
+  @Override
+  public final Object getQoS ()
+  {
+    return super.getQoS ();
+  }
+
+  /** Calls super method (in order to make implementation final).
+   * 
+   * @return The result from the super method.
+   * 
+   */
+  @Override
+  public final Class getQoSClass ()
+  {
+    return super.getQoSClass ();
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
   // RESET
   //
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -132,7 +155,184 @@ public class EncHS
   {
     super.resetEntitySubClass ();
   }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // QUEUE-ACCESS VACATION
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   
+  /** Calls super method (in order to make implementation final).
+   * 
+   */
+  @Override
+  protected final void queueAccessVacationDropSubClass (double time, J job)
+  {
+    super.queueAccessVacationDropSubClass (time, job);
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // ARRIVAL
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  /** Calls super method (in order to make implementation final).
+   * 
+   */
+  @Override
+  protected final void insertJobInQueueUponArrival (final J job, final double time)
+  {
+    super.insertJobInQueueUponArrival (job, time);
+  }
+
+  /** Calls super method (in order to make implementation final).
+   * 
+   */
+  @Override
+  protected final void rescheduleAfterArrival (final J job, final double time)
+  {
+    super.rescheduleAfterArrival (job, time);
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // DROP
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  /** Calls super method (in order to make implementation final).
+   * 
+   */
+  @Override
+  protected final void removeJobFromQueueUponDrop (final J job, final double time)
+  {
+    super.removeJobFromQueueUponDrop (job, time);
+  }
+
+  /** Calls super method (in order to make implementation final).
+   * 
+   */
+  @Override
+  protected final void rescheduleAfterDrop (final J job, final double time)
+  {
+    super.rescheduleAfterDrop (job, time);
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // REVOCATION
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  /** Calls super method (in order to make implementation final).
+   * 
+   */
+  @Override
+  protected final void removeJobFromQueueUponRevokation (final J job, final double time, final boolean auto)
+  {
+    super.removeJobFromQueueUponRevokation (job, time, auto);
+  }
+
+  /** Calls super method (in order to make implementation final).
+   * 
+   */
+  @Override
+  protected final void rescheduleAfterRevokation (final J job, final double time, final boolean auto)
+  {
+    super.rescheduleAfterRevokation (job, time, auto);
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // StartArmed
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  /** Returns {@code false}, since (real) jobs cannot start.
+   * 
+   * <p>
+   * Note: This overrides the default {@link AbstractEncapsulatorSimQueue} implementation.
+   * 
+   * @return {@code false}, since (real) jobs cannot start.
+   * 
+   */
+  @Override
+  public final boolean isStartArmed ()
+  {
+    return false;
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // SERVER-ACCESS CREDITS
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  /** Does nothing.
+   * 
+   * <p>
+   * Note: This overrides the default {@link AbstractEncapsulatorSimQueue} implementation.
+   * 
+   * <p>
+   * In the {@link EncHS} queue, server-access credits on the encapsulated queue are
+   * left untouched.
+   * The composite queue has its own little private notion of the server-access credits,
+   * but since jobs do not start, these do not hurt.
+   * We have to do this in order to prevent all sorts of server-access-credits-related
+   * notifications to enter the composite queue and its listeners.
+   * They might fail to understand why server-access credits change in the absence of
+   * starting jobs.
+   * 
+   */
+  @Override
+  protected final void setServerAccessCreditsSubClass ()
+  {
+  }
+  
+  /** Calls super method (in order to make implementation final).
+   * 
+   */
+  @Override
+  protected final void rescheduleForNewServerAccessCredits (final double time)
+  {
+    super.rescheduleForNewServerAccessCredits (time);
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // START
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  /** Throws an exception.
+   * 
+   * <p>
+   * Note: This overrides the default {@link AbstractEncapsulatorSimQueue} implementation.
+   * 
+   * @throws IllegalStateException Since jobs cannot start.
+   * 
+   */
+  @Override
+  protected final void insertJobInQueueUponStart (final J job, final double time)
+  {
+    throw new IllegalStateException ();
+  }
+
+  /** Throws an exception.
+   * 
+   * <p>
+   * Note: This overrides the default {@link AbstractEncapsulatorSimQueue} implementation.
+   * 
+   * @throws IllegalStateException Since jobs cannot start.
+   * 
+   */
+  @Override
+  protected final void rescheduleAfterStart (final J job, final double time)
+  {
+    throw new IllegalStateException ();
+  }
+
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   //
   // SERVICE TIME FOR JOB
@@ -148,6 +348,57 @@ public class EncHS
   protected final double getServiceTimeForJob (final J job)
   {
     return super.getServiceTimeForJob (job);
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // DEPARTURE
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  /** Calls super method (in order to make implementation final).
+   * 
+   */
+  @Override
+  protected final void removeJobFromQueueUponDeparture (final J departingJob, final double time)
+  {
+    super.removeJobFromQueueUponDeparture (departingJob, time);
+  }
+
+  /** Calls super method (in order to make implementation final).
+   * 
+   */
+  @Override
+  protected final void rescheduleAfterDeparture (final J departedJob, final double time)
+  {
+    super.rescheduleAfterDeparture (departedJob, time);
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // SUB-QUEUE STATE-CHANGE NOTIFICATIONS
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  /** Removes all start notifications from the list and calls super method (and makes the implementation final).
+   * 
+   * @see SimJQSimpleEventType#START
+   * 
+   */
+  @Override
+  protected final void processSubQueueNotifications
+  (final List<MultiSimQueueNotificationProcessor.Notification<DJ, DQ>> notifications)
+  {
+    if (notifications != null)
+      for (MultiSimQueueNotificationProcessor.Notification<DJ, DQ> notification : notifications)
+      {
+        final Iterator<Map<SimEntitySimpleEventType.Member, SimJQEvent<DJ, DQ>>> i_sub
+          = notification.getSubNotifications ().iterator ();
+        while (i_sub.hasNext ())
+          if (i_sub.next ().containsKey (SimJQSimpleEventType.START))
+            i_sub.remove ();
+      }
+    super.processSubQueueNotifications (notifications);
   }
   
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
