2f047a1bc92719b50c835d9d725277ce3e4d4464
Jan de Jongh <jfcmdejongh@gmail.com>
2017-06-25 17:41:46 +0000

    Added test for LCFS_B. [PRESUMED CLEAN COMMIT]
    git-svn-id: https://jandejongh.dnsdojo.org:3690/jqueues/trunk@1063 23c8eed1-2f9f-4d5d-b507-dd3313c27e4c

---
+++ java/test/nl/jdj/jqueues/r5/entity/jq/queue/nonpreemptive/LCFS_BTest.java
@@ -0,0 +1,80 @@
+package nl.jdj.jqueues.r5.entity.jq.queue.nonpreemptive;
+
+import java.util.Collections;
+import java.util.Set;
+import nl.jdj.jqueues.r5.entity.jq.queue.DefaultSimQueueTests;
+import nl.jdj.jqueues.r5.util.loadfactory.LoadFactoryHint;
+import nl.jdj.jqueues.r5.util.loadfactory.pattern.LoadFactory_SQ_SV_0010;
+import nl.jdj.jqueues.r5.util.predictor.SimQueuePredictionException;
+import nl.jdj.jqueues.r5.util.predictor.SimQueuePredictor;
+import nl.jdj.jqueues.r5.util.predictor.queues.SimQueuePredictor_LCFS_B;
+import nl.jdj.jsimulation.r5.DefaultSimEvent;
+import nl.jdj.jsimulation.r5.DefaultSimEventList;
+import nl.jdj.jsimulation.r5.SimEventList;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/** Tests for {@link LCFS_B}.
+ *
+ * @author Jan de Jongh, TNO
+ * 
+ * <p>
+ * Copyright (C) 2005-2017 Jan de Jongh, TNO
+ * 
+ * <p>
+ * This file is covered by the LICENSE file in the root of this project.
+ * 
+ */
+public class LCFS_BTest
+{
+  
+  public LCFS_BTest ()
+  {
+  }
+  
+  @BeforeClass
+  public static void setUpClass ()
+  {
+  }
+  
+  @AfterClass
+  public static void tearDownClass ()
+  {
+  }
+  
+  @Before
+  public void setUp ()
+  {
+  }
+  
+  @After
+  public void tearDown ()
+  {
+  }
+
+  /**
+   * Test of LCFS_B.
+   * 
+   */
+  @Test
+  public void testLCFS_B () throws SimQueuePredictionException
+  {
+    final SimEventList eventList = new DefaultSimEventList (DefaultSimEvent.class);
+    final boolean silent = true;
+    final boolean deadSilent = true;
+    final int numberOfJobs = 50;
+    final Set<LoadFactoryHint> jitterHint = Collections.singleton (LoadFactory_SQ_SV_0010.SERVICE_TIME_JITTER);
+    final int[] bValues = { 0, 1, 2, 100 };
+    for (final int B : bValues)
+    {
+      final LCFS_B queue = new LCFS_B (eventList, B);
+      final SimQueuePredictor predictor = new SimQueuePredictor_LCFS_B (B);
+      DefaultSimQueueTests.doSimQueueTests_SQ_SV
+        (queue, predictor, null, numberOfJobs, jitterHint, silent, deadSilent, 1.0e-12, null, null, null);
+    }
+  }
+
+}
